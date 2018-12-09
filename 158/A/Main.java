308fbf0b741ad6652323e90c1edb1e524297d87d
Kai Wang <charles.wangkai@gmail.com>
2018-12-09 23:50:32 +0800

    Next Round

---
+++ 158/A/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		int[] a = new int[n];
+		for (int i = 0; i < a.length; i++) {
+			a[i] = sc.nextInt();
+		}
+		System.out.println(solve(a, k));
+
+		sc.close();
+	}
+
+	static int solve(int[] a, int k) {
+		int result = 0;
+		while (result < a.length && a[result] > 0 && a[result] >= a[k - 1]) {
+			result++;
+		}
+		return result;
+	}
+}
