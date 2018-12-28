112242c5f04ecb990a0e084cd0e010eb540b2480
Kai Wang <charles.wangkai@gmail.com>
2018-12-28 16:15:13 +0800

    Buttons

---
+++ 268/B/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		System.out.println(solve(n));
+
+		sc.close();
+	}
+
+	static int solve(int n) {
+		int result = n;
+		for (int i = 1; i < n; i++) {
+			result += i * (n - i);
+		}
+		return result;
+	}
+}
