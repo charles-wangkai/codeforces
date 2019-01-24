c625564b24a51ba85f1c252acb007194df69e544
Kai Wang <charles.wangkai@gmail.com>
2019-01-24 15:29:25 +0800

    Again Twenty Five!

---
+++ 630/A/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		long n = sc.nextLong();
+		System.out.println(solve(n));
+
+		sc.close();
+	}
+
+	static int solve(long n) {
+		return 25;
+	}
+}
