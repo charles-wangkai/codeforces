113dab4fd601c42a29901dead6bd8a8fecf2ac19
Kai Wang <charles.wangkai@gmail.com>
2019-01-29 11:08:55 +0800

    Free Ice Cream

---
+++ 686/A/Main.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int x = sc.nextInt();
+		Record[] records = new Record[n];
+		for (int i = 0; i < records.length; i++) {
+			char sign = sc.next().charAt(0);
+			int amount = sc.nextInt();
+
+			records[i] = new Record(sign, amount);
+		}
+		System.out.println(solve(records, x));
+
+		sc.close();
+	}
+
+	static String solve(Record[] records, int x) {
+		long balance = x;
+		int distressNum = 0;
+		for (Record record : records) {
+			if (record.sign == '+' || balance >= record.amount) {
+				if (record.sign == '+') {
+					balance += record.amount;
+				} else {
+					balance -= record.amount;
+				}
+			} else {
+				distressNum++;
+			}
+		}
+		return String.format("%d %d", balance, distressNum);
+	}
+}
+
+class Record {
+	char sign;
+	int amount;
+
+	Record(char sign, int amount) {
+		this.sign = sign;
+		this.amount = amount;
+	}
+}
