fcc086f01f374cccf85c8a6ed6b9b63f06ff543c
Kai Wang <charles.wangkai@gmail.com>
2019-01-31 16:25:39 +0800

    Maximum Increase

---
+++ 702/A/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int[] a = new int[n];
+		for (int i = 0; i < a.length; i++) {
+			a[i] = sc.nextInt();
+		}
+		System.out.println(solve(a));
+
+		sc.close();
+	}
+
+	static int solve(int[] a) {
+		int maxLength = 0;
+		int length = -1;
+		for (int i = 0; i < a.length; i++) {
+			if (i > 0 && a[i] > a[i - 1]) {
+				length++;
+			} else {
+				length = 1;
+			}
+
+			maxLength = Math.max(maxLength, length);
+		}
+		return maxLength;
+	}
+}
