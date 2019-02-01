54acdb34913f3d3ec47f9f879b716d03733bdcf8
Kai Wang <charles.wangkai@gmail.com>
2019-02-01 12:52:12 +0800

    Sale

---
+++ 34/B/Main.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int[] a = new int[n];
+		for (int i = 0; i < a.length; i++) {
+			a[i] = sc.nextInt();
+		}
+		System.out.println(solve(a, m));
+
+		sc.close();
+	}
+
+	static int solve(int[] a, int m) {
+		return -Arrays.stream(a).filter(x -> x < 0).sorted().limit(m).sum();
+	}
+}
