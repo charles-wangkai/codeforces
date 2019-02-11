f074d15be963f0c4d594282ed65e3b62bfd97687
Kai Wang <charles.wangkai@gmail.com>
2019-02-11 15:32:23 +0800

    Mike and palindrome

---
+++ 798/A/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String s = sc.next();
+		System.out.println(solve(s) ? "YES" : "NO");
+
+		sc.close();
+	}
+
+	static boolean solve(String s) {
+		int diffCount = 0;
+		for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
+			if (s.charAt(i) != s.charAt(j)) {
+				diffCount++;
+			}
+		}
+		return diffCount == 1 || (diffCount == 0 && s.length() % 2 != 0);
+	}
+}
