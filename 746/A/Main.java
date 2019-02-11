6847092fc28cd9e817aa823337bfb36a93cccc1d
Kai Wang <charles.wangkai@gmail.com>
2019-02-11 15:02:26 +0800

    Compote

---
+++ 746/A/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		System.out.println(solve(a, b, c));
+
+		sc.close();
+	}
+
+	static int solve(int a, int b, int c) {
+		return Math.min(Math.min(a, b / 2), c / 4) * 7;
+	}
+}
