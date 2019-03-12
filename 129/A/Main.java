385022df632289d6c95cd37152e3cf30bcb06b53
Kai Wang <charles.wangkai@gmail.com>
2019-03-12 12:01:11 +0800

    Cookies

---
+++ 129/A/Main.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int[] a = new int[n];
+		for (int i = 0; i < a.length; i++) {
+			a[i] = sc.nextInt();
+		}
+		System.out.println(solve(a));
+
+		sc.close();
+	}
+
+	static int solve(int[] a) {
+		int sum = Arrays.stream(a).sum();
+
+		return (int) Arrays.stream(a).filter(x -> (sum - x) % 2 == 0).count();
+	}
+}
