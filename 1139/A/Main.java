58c7ea78ec42f58d1e4eeb2728bc3e44f0133ef8
Kai Wang <charles.wangkai@gmail.com>
2019-03-29 11:09:56 +0800

    Even Substrings

---
+++ 1139/A/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		sc.nextInt();
+		String s = sc.next();
+		System.out.println(solve(s));
+
+		sc.close();
+	}
+
+	static int solve(String s) {
+		return IntStream.range(0, s.length()).filter(i -> (s.charAt(i) - '0') % 2 == 0).map(i -> i + 1).sum();
+	}
+}
