00969998a64960239cfa4ac51a6ca9ff2b0f2027
Kai Wang <charles.wangkai@gmail.com>
2019-04-09 13:31:14 +0800

    Lucky Numbers

---
+++ 630/C/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		System.out.println(solve(n));
+
+		sc.close();
+	}
+
+	static long solve(int n) {
+		return (1L << (n + 1)) - 2;
+	}
+}
