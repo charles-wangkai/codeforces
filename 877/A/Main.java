b92a1657f6e935bb8977ebc298744c178ea5abb1
Kai Wang <charles.wangkai@gmail.com>
2019-04-11 12:54:27 +0800

    Alex and broken contest

---
+++ 877/A/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String name = sc.next();
+		System.out.println(solve(name) ? "YES" : "NO");
+
+		sc.close();
+	}
+
+	static boolean solve(String name) {
+		return IntStream.range(0, name.length())
+				.filter(beginIndex -> Stream.of("Danil", "Olya", "Slava", "Ann", "Nikita")
+						.anyMatch(friend -> name.substring(beginIndex).startsWith(friend)))
+				.count() == 1;
+	}
+}
