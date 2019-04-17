82958a8c2e83fac7115529c8f3457a5d9d3d8489
Kai Wang <charles.wangkai@gmail.com>
2019-04-17 12:09:57 +0800

    Professor GukiZ's Robot

---
+++ 620/A/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int x1 = sc.nextInt();
+		int y1 = sc.nextInt();
+		int x2 = sc.nextInt();
+		int y2 = sc.nextInt();
+		System.out.println(solve(x1, y1, x2, y2));
+
+		sc.close();
+	}
+
+	static int solve(int x1, int y1, int x2, int y2) {
+		return Math.max(Math.abs(x1 - x2), Math.abs(y1 - y2));
+	}
+}
