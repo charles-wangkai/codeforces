38807526e4e8d866b40687e40f520df3e4f52970
Kai Wang <charles.wangkai@gmail.com>
2019-04-23 14:02:36 +0800

    Godsend

---
+++ 841/B/Main.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int[] a = new int[n];
+		for (int i = 0; i < a.length; i++) {
+			a[i] = sc.nextInt();
+		}
+		System.out.println(solve(a));
+
+		sc.close();
+	}
+
+	static String solve(int[] a) {
+		return Arrays.stream(a).anyMatch(x -> x % 2 != 0) ? "First" : "Second";
+	}
+}
