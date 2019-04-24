edd380119851441716e2e997b5bf111e38fd6fcc
Kai Wang <charles.wangkai@gmail.com>
2019-04-24 12:48:38 +0800

    Nastya Is Playing Computer Games

---
+++ 1136/B/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		System.out.println(solve(n, k));
+
+		sc.close();
+	}
+
+	static int solve(int n, int k) {
+		return n + (Math.min(k - 1, n - k) + (n - 1)) + (n + 1);
+	}
+}
