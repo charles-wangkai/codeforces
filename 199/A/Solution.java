3ddc545a84a0031785c302d7514f7e915e15400d
Kai Wang <charles.wangkai@gmail.com>
2019-04-28 11:53:23 +0800

    Hexadecimal's theorem

---
+++ 199/A/Solution.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		System.out.println(solve(n));
+
+		sc.close();
+	}
+
+	static String solve(int n) {
+		if (n == 0) {
+			return "0 0 0";
+		}
+
+		int prevPrev = 0;
+		int prev = 1;
+		while (prevPrev + prev != n) {
+			int curr = prevPrev + prev;
+
+			prevPrev = prev;
+			prev = curr;
+		}
+		return String.format("0 %d %d", prevPrev, prev);
+	}
+}
