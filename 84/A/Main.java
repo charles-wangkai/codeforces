02039a61bb552c54875816faea335880dc680a92
Kai Wang <charles.wangkai@gmail.com>
2019-05-06 13:22:08 +0800

    Toy Army

---
+++ 84/A/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		System.out.println(solve(n));
+
+		sc.close();
+	}
+
+	static int solve(int n) {
+		return n / 2 * 3;
+	}
+}
