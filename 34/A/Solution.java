0376730634541510e1c82d3548b8fabe0a793e5a
Kai Wang <charles.wangkai@gmail.com>
2019-05-08 14:58:53 +0800

    Reconnaissance 2

---
+++ 34/A/Solution.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int[] a = new int[n];
+		for (int i = 0; i < a.length; i++) {
+			a[i] = sc.nextInt();
+		}
+		System.out.println(solve(a));
+
+		sc.close();
+	}
+
+	static String solve(int[] a) {
+		int bestIndex = 0;
+		for (int i = 1; i < a.length; i++) {
+			if (Math.abs(a[(i + 1) % a.length] - a[i]) < Math.abs(a[(bestIndex + 1) % a.length] - a[bestIndex])) {
+				bestIndex = i;
+			}
+		}
+
+		return String.format("%d %d", bestIndex + 1, (bestIndex + 1) % a.length + 1);
+	}
+}
