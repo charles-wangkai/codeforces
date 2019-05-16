a8d58d25d2659f9970845150414f411cbae40835
Kai Wang <charles.wangkai@gmail.com>
2019-05-16 12:41:59 +0800

    Quirky Quantifiers

---
+++ 952/A/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+		System.out.println(solve(a));
+
+		sc.close();
+	}
+
+	static int solve(int a) {
+		return a % 2;
+	}
+}
