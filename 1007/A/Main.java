7e28e0494b010e9af9236805f38e30fce3c62f22
Kai Wang <charles.wangkai@gmail.com>
2019-05-17 13:53:36 +0800

    Reorder the Array

---
+++ 1007/A/Main.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int[] a = new int[n];
+		for (int i = 0; i < a.length; i++) {
+			a[i] = sc.nextInt();
+		}
+		System.out.println(solve(a));
+
+		sc.close();
+	}
+
+	static int solve(int[] a) {
+		Arrays.sort(a);
+
+		int result = 0;
+		int indexForSmaller = 0;
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] > a[indexForSmaller]) {
+				result++;
+				indexForSmaller++;
+			}
+		}
+		return result;
+	}
+}
