40f8b74d564cc70240439324d42c89c4e4f30048
Kai Wang <charles.wangkai@gmail.com>
2019-05-21 18:35:45 +0800

    Alyona and Mex

---
+++ 682/B/Main.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int[] a = new int[n];
+		for (int i = 0; i < a.length; i++) {
+			a[i] = sc.nextInt();
+		}
+		System.out.println(solve(a));
+
+		sc.close();
+	}
+
+	static int solve(int[] a) {
+		int[] sortedA = Arrays.stream(a).boxed().sorted().mapToInt(x -> x).toArray();
+
+		int result = 1;
+		for (int ai : sortedA) {
+			if (ai >= result) {
+				result++;
+			}
+		}
+		return result;
+	}
+}
