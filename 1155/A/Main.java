127b1eb2d5dc72150a38f16a4ff4200910e36a26
Kai Wang <charles.wangkai@gmail.com>
2019-05-22 15:12:14 +0800

    Reverse a Substring

---
+++ 1155/A/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		sc.nextInt();
+		String s = sc.next();
+		System.out.print(solve(s));
+
+		sc.close();
+	}
+
+	static String solve(String s) {
+		for (int i = 0; i < s.length() - 1; i++) {
+			if (s.charAt(i) > s.charAt(i + 1)) {
+				return String.format("YES\n%d %d", i + 1, i + 2);
+			}
+		}
+
+		return "NO";
+	}
+}
