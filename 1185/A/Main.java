a2d34db66edde4a4b7accbe8e6ecce69cff46f5f
Kai Wang <charles.wangkai@gmail.com>
2019-06-26 10:57:37 +0800

    Ropewalkers

---
+++ 1185/A/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int d = sc.nextInt();
+		System.out.println(solve(a, b, c, d));
+
+		sc.close();
+	}
+
+	static int solve(int a, int b, int c, int d) {
+		int[] sorted = IntStream.of(a, b, c).sorted().toArray();
+
+		return Math.max(0, d - (sorted[1] - sorted[0])) + Math.max(0, d - (sorted[2] - sorted[1]));
+	}
+}
