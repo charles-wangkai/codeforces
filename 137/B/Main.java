2011757d0c89f007dcbd9c097df712c93815a128
Kai Wang <charles.wangkai@gmail.com>
2019-08-22 12:12:41 +0800

    Permutation

---
+++ 137/B/Main.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int[] a = new int[n];
+		for (int i = 0; i < a.length; i++) {
+			a[i] = sc.nextInt();
+		}
+		System.out.println(solve(a));
+
+		sc.close();
+	}
+
+	static int solve(int[] a) {
+		return a.length - (int) Arrays.stream(a).filter(x -> x <= a.length).distinct().count();
+	}
+}
