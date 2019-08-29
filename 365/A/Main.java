316114c5cb72eba25df90181813df0c31f7a47e3
Kai Wang <charles.wangkai@gmail.com>
2019-08-29 16:44:08 +0800

    Good Number

---
+++ 365/A/Main.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		int[] a = new int[n];
+		for (int i = 0; i < a.length; i++) {
+			a[i] = sc.nextInt();
+		}
+		System.out.println(solve(a, k));
+
+		sc.close();
+	}
+
+	static int solve(int[] a, int k) {
+		return (int) Arrays.stream(a)
+				.filter(x -> String.valueOf(x).chars().filter(ch -> ch - '0' <= k).distinct().count() == k + 1).count();
+	}
+}
