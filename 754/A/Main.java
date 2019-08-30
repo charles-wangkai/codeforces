11155f6329f4c156a92b593169031cc9663ea99d
Kai Wang <charles.wangkai@gmail.com>
2019-08-30 10:22:51 +0800

    Rename class name

--- 754/A/Main.java
+++ 754/A/Main.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 import java.util.stream.IntStream;
 
-public class Solution {
+public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
