f0b8f68ba4af065180fae47a989bfa99bf36ac31
Kai Wang <charles.wangkai@gmail.com>
2019-11-05 18:47:29 +0800

    Catch Overflow!

---
+++ 1175/B/Main.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    static final long LIMIT = (1L << 32) - 1;
+
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+
+	int l = sc.nextInt();
+	sc.nextLine();
+	String[] commands = new String[l];
+	for (int i = 0; i < commands.length; i++) {
+	    commands[i] = sc.nextLine();
+	}
+	System.out.println(solve(commands));
+
+	sc.close();
+    }
+
+    static String solve(String[] commands) {
+	Stack<Element> stack = new Stack<>();
+	stack.push(new Element(-1, 0));
+
+	for (String command : commands) {
+	    if (command.startsWith("for")) {
+		int loop = Integer.parseInt(command.split(" ")[1]);
+
+		stack.push(new Element(loop, 0));
+	    } else if (command.equals("end")) {
+		Element element = stack.pop();
+
+		stack.peek().amount += element.loop * element.amount;
+	    } else {
+		stack.peek().amount++;
+	    }
+
+	    if (stack.peek().amount > LIMIT) {
+		return "OVERFLOW!!!";
+	    }
+	}
+
+	return String.valueOf(stack.peek().amount);
+    }
+}
+
+class Element {
+    int loop;
+    long amount;
+
+    Element(int loop, long amount) {
+	this.loop = loop;
+	this.amount = amount;
+    }
+}
