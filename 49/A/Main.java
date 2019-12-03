8781c57665bbb4344e02113b375282d9b9f80745
Kai Wang <charles.wangkai@gmail.com>
2019-12-03 18:17:21 +0800

    Sleuth

---
+++ 49/A/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String question = sc.nextLine();
+		System.out.println(solve(question) ? "YES" : "NO");
+
+		sc.close();
+	}
+
+	static boolean solve(String question) {
+		return "aeiouy"
+				.indexOf(new StringBuilder(question.toLowerCase().replaceAll(" |\\?", "")).reverse().charAt(0)) >= 0;
+	}
+}
