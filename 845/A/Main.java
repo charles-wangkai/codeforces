7ebf011604742103f72438918d3370eb4fc2ae45
Kai Wang <charles.wangkai@gmail.com>
2019-12-24 04:15:58 +0800

    Chess Tourney

---
+++ 845/A/Main.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int[] a = new int[n * 2];
+		for (int i = 0; i < a.length; ++i) {
+			a[i] = sc.nextInt();
+		}
+		System.out.println(solve(a) ? "YES" : "NO");
+
+		sc.close();
+	}
+
+	static boolean solve(int[] a) {
+		int[] sorted = Arrays.stream(a).boxed().sorted().mapToInt(x -> x).toArray();
+
+		return sorted[a.length / 2 - 1] != sorted[a.length / 2];
+	}
+}
