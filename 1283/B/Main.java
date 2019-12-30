091fba4666646f8ef4a95cafbf393cdc1e2fb479
Kai Wang <charles.wangkai@gmail.com>
2019-12-30 22:28:37 +0800

    Candies Division

---
+++ 1283/B/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int t = sc.nextInt();
+		for (int tc = 0; tc < t; ++tc) {
+			int n = sc.nextInt();
+			int k = sc.nextInt();
+
+			System.out.println(solve(n, k));
+		}
+
+		sc.close();
+	}
+
+	static int solve(int n, int k) {
+		return n / k * k + Math.min(k / 2, n % k);
+	}
+}
