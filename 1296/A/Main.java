ecd9fe2622dfec47f49ef50c951bbe821a60b9ba
Kai Wang <charles.wangkai@gmail.com>
2020-02-05 23:59:14 +0800

    Array with Odd Sum

---
+++ 1296/A/Main.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int t = sc.nextInt();
+		for (int tc = 0; tc < t; ++tc) {
+			int n = sc.nextInt();
+			int[] a = new int[n];
+			for (int i = 0; i < a.length; ++i) {
+				a[i] = sc.nextInt();
+			}
+
+			System.out.println(solve(a) ? "YES" : "NO");
+		}
+
+		sc.close();
+	}
+
+	static boolean solve(int[] a) {
+		return Arrays.stream(a).map(x -> x % 2).distinct().count() == 2 || Arrays.stream(a).sum() % 2 != 0;
+	}
+}
