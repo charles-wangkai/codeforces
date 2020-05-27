f5b01c62af8f19109c080c853ba5e62c604d85e2
Kai Wang <charles.wangkai@gmail.com>
2020-05-27 10:07:55 +0800

    Maria Breaks the Self-isolation

---
+++ 1358/B/Main.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        for (int tc = 0; tc < t; ++tc) {
+            int n = sc.nextInt();
+            int[] a = new int[n];
+            for (int i = 0; i < a.length; ++i) {
+                a[i] = sc.nextInt();
+            }
+
+            System.out.println(solve(a));
+        }
+
+        sc.close();
+    }
+
+    static int solve(int[] a) {
+        a = Arrays.stream(a).boxed().sorted().mapToInt(x -> x).toArray();
+
+        for (int i = a.length - 1; i >= 0; --i) {
+            if (a[i] <= i + 1) {
+                return i + 2;
+            }
+        }
+
+        return 1;
+    }
+}
