42673fee421b8e07974a86e6da02ddf9125d5e12
Kai Wang <charles.wangkai@gmail.com>
2020-05-29 04:36:39 +0800

    Berland Poker

---
+++ 1359/A/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        for (int tc = 0; tc < t; ++tc) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            int k = sc.nextInt();
+
+            System.out.println(solve(n, m, k));
+        }
+
+        sc.close();
+    }
+
+    static int solve(int n, int m, int k) {
+        int first = Math.min(m, n / k);
+        int second = (m - first + k - 2) / (k - 1);
+
+        return first - second;
+    }
+}
