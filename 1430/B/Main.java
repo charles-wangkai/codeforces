3fcfff5da8e2bdb1541c66cf3cb9c456e06e68e9
Kai Wang <charles.wangkai@gmail.com>
2020-10-17 00:36:05 +0800

    Barrels

---
+++ 1430/B/Main.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int k = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a, k));
+    }
+
+    sc.close();
+  }
+
+  static long solve(int[] a, int k) {
+    return Arrays.stream(a)
+        .boxed()
+        .sorted(Collections.reverseOrder())
+        .limit(k + 1)
+        .mapToLong(x -> x)
+        .sum();
+  }
+}
