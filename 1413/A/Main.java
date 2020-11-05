d21e363675e811469223329c118d3e560bededbe
Kai Wang <charles.wangkai@gmail.com>
2020-11-05 21:13:04 +0800

    Finding Sasuke

---
+++ 1413/A/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int T = sc.nextInt();
+    for (int tc = 0; tc < T; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a));
+    }
+
+    sc.close();
+  }
+
+  static String solve(int[] a) {
+    return IntStream.range(0, a.length)
+        .mapToObj(i -> String.valueOf((i % 2 == 0) ? a[i + 1] : -a[i - 1]))
+        .collect(Collectors.joining(" "));
+  }
+}
