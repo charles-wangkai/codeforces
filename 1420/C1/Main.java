e8c03625d3ea5173ca79524429a28255b1ae000d
Kai Wang <charles.wangkai@gmail.com>
2020-11-07 13:56:38 +0800

    Pokémon Army (easy version)

---
+++ 1420/C1/Main.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a));
+    }
+
+    sc.close();
+  }
+
+  static long solve(int[] a) {
+    int[] extended = Arrays.copyOf(a, a.length + 1);
+
+    return IntStream.range(0, extended.length - 1)
+        .map(i -> Math.max(0, extended[i] - extended[i + 1]))
+        .asLongStream()
+        .sum();
+  }
+}
