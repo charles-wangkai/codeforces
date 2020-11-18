38ba401cfef8af9f4bfd06368dec9b965e9ef835
Kai Wang <charles.wangkai@gmail.com>
2020-11-18 18:07:07 +0800

    Specific Tastes of Andre

---
+++ 1438/A/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+
+      System.out.println(solve(n));
+    }
+
+    sc.close();
+  }
+
+  static String solve(int n) {
+    return IntStream.range(0, n).mapToObj(i -> "1").collect(Collectors.joining(" "));
+  }
+}
