31e0fd0eb60cc1daf96a5a5d720f45d0befdc8f7
Kai Wang <charles.wangkai@gmail.com>
2020-12-15 23:22:06 +0800

    [Watermelon] reformat

--- 4/A/Main.java
+++ 4/A/Main.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
 
-		int w = sc.nextInt();
-		System.out.println(solve(w) ? "YES" : "NO");
+    int w = sc.nextInt();
+    System.out.println(solve(w) ? "YES" : "NO");
 
-		sc.close();
-	}
+    sc.close();
+  }
 
-	static boolean solve(int w) {
-		return w >= 4 && w % 2 == 0;
-	}
+  static boolean solve(int w) {
+    return w >= 4 && w % 2 == 0;
+  }
 }
