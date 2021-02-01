61d4f0cf144a5862a5b71d55a36ccff1f2bcd97f
Kai Wang <charles.wangkai@gmail.com>
2021-02-01 08:20:34 +0800

    Nezzar and Colorful Balls

---
+++ 1478/A/Main.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int[] a) {
+    int[] counts = new int[a.length + 1];
+    for (int ai : a) {
+      ++counts[ai];
+    }
+
+    return Arrays.stream(counts).max().getAsInt();
+  }
+}
