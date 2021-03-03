811d2d012343cea1b1effb324b940bb955347729
Kai Wang <charles.wangkai@gmail.com>
2021-03-03 16:45:37 +0800

    Water Buying

--- 1118/A/Main.java
+++ 1118/A/Main.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
 
-		int q = sc.nextInt();
-		for (int tc = 0; tc < q; tc++) {
-			long n = sc.nextLong();
-			int a = sc.nextInt();
-			int b = sc.nextInt();
+    int q = sc.nextInt();
+    for (int tc = 0; tc < q; ++tc) {
+      long n = sc.nextLong();
+      int a = sc.nextInt();
+      int b = sc.nextInt();
 
-			System.out.println(solve(n, a, b));
-		}
+      System.out.println(solve(n, a, b));
+    }
 
-		sc.close();
-	}
+    sc.close();
+  }
 
-	static long solve(long n, int a, int b) {
-		return (n / 2 * Math.min(2 * a, b)) + (n % 2 * a);
-	}
+  static long solve(long n, int a, int b) {
+    return (n / 2 * Math.min(2 * a, b)) + (n % 2 * a);
+  }
 }
