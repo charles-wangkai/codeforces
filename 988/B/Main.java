e5113bdc2724992af8db55e936adcd206cf74ae0
Kai Wang <charles.wangkai@gmail.com>
2021-03-03 16:58:31 +0800

    Substrings Sort

--- 988/B/Main.java
+++ 988/B/Main.java
@@ -1,28 +1,29 @@
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 import java.util.stream.IntStream;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
 
-		int n = sc.nextInt();
-		String[] strings = new String[n];
-		for (int i = 0; i < strings.length; i++) {
-			strings[i] = sc.next();
-		}
-		System.out.println(solve(strings));
+    int n = sc.nextInt();
+    String[] strings = new String[n];
+    for (int i = 0; i < strings.length; ++i) {
+      strings[i] = sc.next();
+    }
+    System.out.println(solve(strings));
 
-		sc.close();
-	}
+    sc.close();
+  }
 
-	static String solve(String[] strings) {
-		Arrays.sort(strings, (s1, s2) -> Integer.compare(s1.length(), s2.length()));
+  static String solve(String[] strings) {
+    Arrays.sort(strings, Comparator.comparing(String::length));
 
-		if (IntStream.range(0, strings.length - 1).allMatch(i -> strings[i + 1].contains(strings[i]))) {
-			return String.format("YES\n%s", String.join("\n", strings));
-		} else {
-			return "NO";
-		}
-	}
+    if (IntStream.range(0, strings.length - 1).allMatch(i -> strings[i + 1].contains(strings[i]))) {
+      return String.format("YES\n%s", String.join("\n", strings));
+    } else {
+      return "NO";
+    }
+  }
 }
