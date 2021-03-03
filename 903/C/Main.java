197ad43721611f6da883cfee09919bf8f8da82e5
Kai Wang <charles.wangkai@gmail.com>
2021-03-03 17:23:07 +0800

    Boxes Packing

--- 903/C/Main.java
+++ 903/C/Main.java
@@ -3,25 +3,25 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
 
-		int n = sc.nextInt();
-		int[] a = new int[n];
-		for (int i = 0; i < a.length; i++) {
-			a[i] = sc.nextInt();
-		}
-		System.out.println(solve(a));
+    int n = sc.nextInt();
+    int[] a = new int[n];
+    for (int i = 0; i < a.length; ++i) {
+      a[i] = sc.nextInt();
+    }
+    System.out.println(solve(a));
 
-		sc.close();
-	}
+    sc.close();
+  }
 
-	static int solve(int[] a) {
-		Map<Integer, Integer> sideToCount = new HashMap<>();
-		for (int side : a) {
-			sideToCount.put(side, sideToCount.getOrDefault(side, 0) + 1);
-		}
+  static int solve(int[] a) {
+    Map<Integer, Integer> sideToCount = new HashMap<>();
+    for (int side : a) {
+      sideToCount.put(side, sideToCount.getOrDefault(side, 0) + 1);
+    }
 
-		return sideToCount.values().stream().mapToInt(x -> x).max().getAsInt();
-	}
+    return sideToCount.values().stream().mapToInt(x -> x).max().getAsInt();
+  }
 }
