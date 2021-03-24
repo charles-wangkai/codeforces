62bf51a4707ef9be5828135f3b384e1064db5143
Kai Wang <charles.wangkai@gmail.com>
2021-03-24 23:26:22 +0800

    Binary Removals

---
+++ 1499/B/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      String s = sc.next();
+
+      System.out.println(solve(s) ? "YES" : "NO");
+    }
+
+    sc.close();
+  }
+
+  static boolean solve(String s) {
+    int index = s.indexOf("11");
+
+    return index == -1 || s.indexOf("00", index + 2) == -1;
+  }
+}
