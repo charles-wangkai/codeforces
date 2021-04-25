22875e103ef2d10cfa063b5b1192c152709ec760
Kai Wang <charles.wangkai@gmail.com>
2021-04-25 21:42:04 +0800

    Perfectly Imperfect Array

---
+++ 1514/A/Main.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a) ? "YES" : "NO");
+    }
+
+    sc.close();
+  }
+
+  static boolean solve(int[] a) {
+    return Arrays.stream(a).anyMatch(x -> !isSquare(x));
+  }
+
+  static boolean isSquare(int x) {
+    int root = (int) Math.round(Math.sqrt(x));
+
+    return root * root == x;
+  }
+}
