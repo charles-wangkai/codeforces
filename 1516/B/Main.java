9f9b955cf4e07f32ca8a6fbcd40f73898f2f0f78
Kai Wang <charles.wangkai@gmail.com>
2021-05-27 00:17:03 +0800

    AGAGA XOOORRR

---
+++ 1516/B/Main.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a) ? "YES" : "NO");
+    }
+
+    sc.close();
+  }
+
+  static boolean solve(int[] a) {
+    int xor = Arrays.stream(a).reduce((x, y) -> x ^ y).getAsInt();
+    if (xor == 0) {
+      return true;
+    }
+
+    int part = 0;
+    int count = 0;
+    for (int ai : a) {
+      part ^= ai;
+      if (part == xor) {
+        part = 0;
+        ++count;
+      }
+    }
+
+    return count >= 2;
+  }
+}
