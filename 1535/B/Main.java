559dba2569a217ee6c0ec8c0783ebe18603685d6
Kai Wang <charles.wangkai@gmail.com>
2021-06-05 10:34:56 +0800

    Array Reodering

---
+++ 1535/B/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int[] a) {
+    int result = 0;
+    for (int i = 0; i < a.length; ++i) {
+      for (int j = i + 1; j < a.length; ++j) {
+        if (gcd(a[i], a[j]) != 1 || (a[i] % 2 != a[j] % 2)) {
+          ++result;
+        }
+      }
+    }
+
+    return result;
+  }
+
+  static int gcd(int x, int y) {
+    return (y == 0) ? x : gcd(y, x % y);
+  }
+}
