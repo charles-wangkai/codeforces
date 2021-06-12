8154d9900f0e6a785a089ef6b6b37740478f3853
Kai Wang <charles.wangkai@gmail.com>
2021-06-12 10:22:11 +0800

    Friends and Candies

---
+++ 1538/B/Main.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int[] a) {
+    int sum = Arrays.stream(a).sum();
+    if (sum % a.length != 0) {
+      return -1;
+    }
+
+    return (int) Arrays.stream(a).filter(x -> x > sum / a.length).count();
+  }
+}
