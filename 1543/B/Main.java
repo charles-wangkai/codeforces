ac973ee62afd4de39c1ad1d4fd73bdf699677618
Kai Wang <charles.wangkai@gmail.com>
2021-07-08 12:07:35 +0800

    Customising the Track

---
+++ 1543/B/Main.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a));
+    }
+
+    sc.close();
+  }
+
+  static long solve(int[] a) {
+    int excessNum = (int) (Arrays.stream(a).asLongStream().sum() % a.length);
+
+    return (long) excessNum * (a.length - excessNum);
+  }
+}
