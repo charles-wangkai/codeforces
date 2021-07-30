1704f650aa987fd7fd9dbb2b6e0d6174d16aac06
Kai Wang <charles.wangkai@gmail.com>
2021-07-30 17:57:19 +0800

    Cherry

---
+++ 1554/A/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a));
+    }
+
+    sc.close();
+  }
+
+  static long solve(int[] a) {
+    return IntStream.range(0, a.length - 1)
+        .mapToLong(i -> (long) a[i] * a[i + 1])
+        .max()
+        .getAsLong();
+  }
+}
