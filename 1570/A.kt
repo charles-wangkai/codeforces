e0a8dbc281d49eefd9df22a2651866f37fcb9b70
Kai Wang <charles.wangkai@gmail.com>
2021-10-03 23:52:23 +0800

    A+B (Trial Problem)

---
+++ 1570/A.kt
@@ -0,0 +1,16 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    val sc = Scanner(System.`in`)
+    val t = sc.nextInt()
+    for (tc in 0 until t) {
+        val a = sc.nextInt()
+        val b = sc.nextInt()
+        println(solve(a, b))
+    }
+    sc.close()
+}
+
+fun solve(a: Int, b: Int): Int {
+    return a + b
+}
