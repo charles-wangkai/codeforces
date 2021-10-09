e11d1a3d69105b47d83d1cd0e1240bb57cfa1500
Kai Wang <charles.wangkai@gmail.com>
2021-10-09 22:06:36 +0800

    Gamer Hemose

--- 1592/A/Main.java
+++ 1592/A/Main.java
@@ -1,24 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+  public static void main(String[] args) throws Throwable {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    int t = sc.nextInt();
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int t = Integer.parseInt(st.nextToken());
     for (int tc = 0; tc < t; ++tc) {
-      int n = sc.nextInt();
-      int H = sc.nextInt();
+      st = new StringTokenizer(br.readLine());
+      int n = Integer.parseInt(st.nextToken());
+      int H = Integer.parseInt(st.nextToken());
       int[] a = new int[n];
+      st = new StringTokenizer(br.readLine());
       for (int i = 0; i < a.length; ++i) {
-        a[i] = sc.nextInt();
+        a[i] = Integer.parseInt(st.nextToken());
       }
 
       System.out.println(solve(a, H));
     }
-
-    sc.close();
   }
 
   static int solve(int[] a, int H) {
