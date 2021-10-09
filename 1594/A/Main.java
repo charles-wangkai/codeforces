851f4740082da1e2360f7930c85073c79d20f2fc
Kai Wang <charles.wangkai@gmail.com>
2021-10-09 09:48:47 +0800

    Consecutive Sum Riddle

---
+++ 1594/A/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      long n = sc.nextLong();
+
+      System.out.println(solve(n));
+    }
+
+    sc.close();
+  }
+
+  static String solve(long n) {
+    return String.format("%d %d", 1 - n, n);
+  }
+}
