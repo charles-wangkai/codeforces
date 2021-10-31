bb999d542eeffd4feaf18736e7764224eb186cd5
Kai Wang <charles.wangkai@gmail.com>
2021-10-31 11:35:18 +0800

    Era

---
+++ 1604/A/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int[] a) {
+    int result = 0;
+    int size = 0;
+    for (int ai : a) {
+      ++size;
+      int added = Math.max(0, ai - size);
+      result += added;
+      size += added;
+    }
+
+    return result;
+  }
+}
