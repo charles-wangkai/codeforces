080d88b955a784eecfa4709f283f3c0f2a831483
Kai Wang <charles.wangkai@gmail.com>
2021-12-21 05:45:28 +0800

    Square String?

---
+++ 1619/A/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      String s = sc.next();
+
+      System.out.println(solve(s) ? "YES" : "NO");
+    }
+
+    sc.close();
+  }
+
+  static boolean solve(String s) {
+    return s.substring(0, s.length() / 2).equals(s.substring(s.length() / 2));
+  }
+}
