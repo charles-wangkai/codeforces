28ebba4670fa8a5bb9f479104fc16b4927433b3c
Kai Wang <charles.wangkai@gmail.com>
2022-01-31 10:45:55 +0800

    Roof Construction

---
+++ 1632/B/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+
+      System.out.println(solve(n));
+    }
+
+    sc.close();
+  }
+
+  static String solve(int n) {
+    int power = Integer.highestOneBit(n - 1);
+
+    return IntStream.concat(
+            IntStream.range(power, n).map(i -> power + n - 1 - i), IntStream.range(0, power))
+        .mapToObj(String::valueOf)
+        .collect(Collectors.joining(" "));
+  }
+}
