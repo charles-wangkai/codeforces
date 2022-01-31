7ebe9f8859080750c83bfbf8c632aaf095be5b26
Kai Wang <charles.wangkai@gmail.com>
2022-01-31 09:43:52 +0800

    ABC

---
+++ 1632/A/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      sc.nextInt();
+      String s = sc.next();
+
+      System.out.println(solve(s) ? "YES" : "NO");
+    }
+
+    sc.close();
+  }
+
+  static boolean solve(String s) {
+    return s.length() == 1 || (s.length() == 2 && s.charAt(0) != s.charAt(1));
+  }
+}
