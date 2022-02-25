921cc3d74f91130a96a80c9879403a14049e68ac
Kai Wang <charles.wangkai@gmail.com>
2022-02-25 23:10:04 +0800

    Doors and Keys

---
+++ 1644/A/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      String s = sc.next();
+
+      System.out.println(solve(s) ? "YES" : "NO");
+    }
+
+    sc.close();
+  }
+
+  static boolean solve(String s) {
+    return s.indexOf('r') < s.indexOf('R')
+        && s.indexOf('g') < s.indexOf('G')
+        && s.indexOf('b') < s.indexOf('B');
+  }
+}
