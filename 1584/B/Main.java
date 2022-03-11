92867197deff425763d52929a3c4614f50c4abf9
Kai Wang <charles.wangkai@gmail.com>
2022-03-11 22:52:51 +0800

    Coloring Rectangles

---
+++ 1584/B/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int m = sc.nextInt();
+
+      System.out.println(solve(n, m));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int n, int m) {
+    return (n * m + 2) / 3;
+  }
+}
