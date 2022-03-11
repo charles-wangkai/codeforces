9beb6f0166d78e669753879f90094ad08bb6612b
Kai Wang <charles.wangkai@gmail.com>
2022-03-11 21:45:34 +0800

    Game

---
+++ 1649/A/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int[] a) {
+    int[] zeroIndices = IntStream.range(0, a.length).filter(i -> a[i] == 0).toArray();
+
+    return (zeroIndices.length == 0)
+        ? 0
+        : (zeroIndices[zeroIndices.length - 1] - zeroIndices[0] + 2);
+  }
+}
