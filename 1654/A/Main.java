360d850aa7727edd1e6a0a318c9f65dba41d640b
Kai Wang <charles.wangkai@gmail.com>
2022-03-21 10:12:39 +0800

    Maximum Cake Tastiness

---
+++ 1654/A/Main.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int[] a) {
+    return Arrays.stream(a)
+        .boxed()
+        .sorted(Comparator.reverseOrder())
+        .limit(2)
+        .mapToInt(x -> x)
+        .sum();
+  }
+}
