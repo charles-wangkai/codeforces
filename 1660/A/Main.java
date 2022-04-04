37eccba94d536048a4ca682635a54f4bfe8e4550
Kai Wang <charles.wangkai@gmail.com>
2022-04-04 16:05:24 +0800

    Vasya and Coins

---
+++ 1660/A/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+
+      System.out.println(solve(a, b));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int a, int b) {
+    return (a == 0) ? 1 : (a + 2 * b + 1);
+  }
+}
