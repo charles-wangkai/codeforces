09428c2847c4b963460d6761049758e069dc5bcc
Kai Wang <charles.wangkai@gmail.com>
2022-04-09 22:17:28 +0800

    GCD vs LCM

---
+++ 1665/A/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+
+      System.out.println(solve(n));
+    }
+
+    sc.close();
+  }
+
+  static String solve(int n) {
+    return String.format("%d 1 1 1", n - 3);
+  }
+}
