2cec3469b0b159894cab7e36d0d86b04a50b820a
Kai Wang <charles.wangkai@gmail.com>
2022-06-01 22:51:24 +0800

    Everything Everywhere All But One

---
+++ 1686/A/Main.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a) ? "YES" : "NO");
+    }
+
+    sc.close();
+  }
+
+  static boolean solve(int[] a) {
+    int sum = Arrays.stream(a).sum();
+
+    return Arrays.stream(a).anyMatch(ai -> ai * a.length == sum);
+  }
+}
