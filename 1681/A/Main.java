7894ea8462d445ae1f861cf5b016ba05b741a55e
Kai Wang <charles.wangkai@gmail.com>
2022-06-01 22:32:08 +0800

    Game with Cards

---
+++ 1681/A/Main.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+      int m = sc.nextInt();
+      int[] b = new int[m];
+      for (int i = 0; i < b.length; ++i) {
+        b[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a, b));
+    }
+
+    sc.close();
+  }
+
+  static String solve(int[] a, int[] b) {
+    int maxA = Arrays.stream(a).max().getAsInt();
+    int maxB = Arrays.stream(b).max().getAsInt();
+
+    return String.format(
+        "%s\n%s", (maxA >= maxB) ? "Alice" : "Bob", (maxB >= maxA) ? "Bob" : "Alice");
+  }
+}
