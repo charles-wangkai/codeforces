f9a1759fad17b6b8a240e123e2ece3736bb122a3
Kai Wang <charles.wangkai@gmail.com>
2022-06-07 18:27:36 +0800

    A-B-C Sort

---
+++ 1674/D/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a) ? "YES" : "NO");
+    }
+
+    sc.close();
+  }
+
+  static boolean solve(int[] a) {
+    for (int i = (a.length % 2 == 0) ? 0 : 1; i < a.length; i += 2) {
+      if (a[i] > a[i + 1]) {
+        int temp = a[i];
+        a[i] = a[i + 1];
+        a[i + 1] = temp;
+      }
+    }
+
+    return IntStream.range(0, a.length - 1).allMatch(i -> a[i] <= a[i + 1]);
+  }
+}
