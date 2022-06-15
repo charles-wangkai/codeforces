5fe8542e5414106680a96b4b3de37821066bd3cb
Kai Wang <charles.wangkai@gmail.com>
2022-06-15 20:42:44 +0800

    Parkway Walk

---
+++ 1697/A/Main.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int m = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a, m));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int[] a, int m) {
+    return Math.max(0, Arrays.stream(a).sum() - m);
+  }
+}
