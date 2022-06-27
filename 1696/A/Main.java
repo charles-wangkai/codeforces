863c6bad220fd3eb555f911e0204dd8fa29ab72d
Kai Wang <charles.wangkai@gmail.com>
2022-06-27 18:24:36 +0800

    NIT orz!

---
+++ 1696/A/Main.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int z = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a, z));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int[] a, int z) {
+    return Arrays.stream(a).map(ai -> ai | z).max().getAsInt();
+  }
+}
