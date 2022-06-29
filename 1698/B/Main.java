3bba3fa4dfdc97fa15cdbff19b0cc3dafa812028
Kai Wang <charles.wangkai@gmail.com>
2022-06-29 19:25:58 +0800

    Rising Sand

---
+++ 1698/B/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int k = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a, k));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int[] a, int k) {
+    return (k == 1)
+        ? ((a.length - 1) / 2)
+        : (int) IntStream.range(1, a.length - 1).filter(i -> a[i] > a[i - 1] + a[i + 1]).count();
+  }
+}
