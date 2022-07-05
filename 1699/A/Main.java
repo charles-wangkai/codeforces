930f5b940813cdfb2216ea8e4b2b2624d648b778
Kai Wang <charles.wangkai@gmail.com>
2022-07-05 20:34:24 +0800

    The Third Three Number Problem

---
+++ 1699/A/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+
+      System.out.println(solve(n));
+    }
+
+    sc.close();
+  }
+
+  static String solve(int n) {
+    return (n % 2 == 0) ? String.format("0 0 %d", n / 2) : "-1";
+  }
+}
