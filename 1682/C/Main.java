2798d3b22c4d305cb2faa05e8aaa5cb90ec294c1
Kai Wang <charles.wangkai@gmail.com>
2022-07-10 11:42:09 +0800

    LIS or Reverse LIS?

---
+++ 1682/C/Main.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int[] a) {
+    Map<Integer, Integer> valueToCount = new HashMap<>();
+    for (int value : a) {
+      valueToCount.put(value, valueToCount.getOrDefault(value, 0) + 1);
+    }
+
+    return (valueToCount.size()
+            + (int) valueToCount.values().stream().filter(count -> count >= 2).count()
+            + 1)
+        / 2;
+  }
+}
