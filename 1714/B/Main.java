d7006149e2c657bd0df91d5d0c011fa1b060c711
Kai Wang <charles.wangkai@gmail.com>
2022-08-02 19:16:23 +0800

    Remove Prefix

---
+++ 1714/B/Main.java
@@ -0,0 +1,33 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int[] a) {
+    Set<Integer> seen = new HashSet<>();
+    int index = a.length;
+    while (index != 0 && !seen.contains(a[index - 1])) {
+      --index;
+      seen.add(a[index]);
+    }
+
+    return index;
+  }
+}
