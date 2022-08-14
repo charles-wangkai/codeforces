9c72b58e23aec26cd430100e2ad85b1a3d99f75e
Kai Wang <charles.wangkai@gmail.com>
2022-08-14 22:03:21 +0800

    Wonderful Permutation

---
+++ 1712/A/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int k = sc.nextInt();
+      int[] p = new int[n];
+      for (int i = 0; i < p.length; ++i) {
+        p[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(p, k));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int[] p, int k) {
+    return (int) IntStream.range(0, k).filter(i -> p[i] > k).count();
+  }
+}
