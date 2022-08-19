450563796ad421214d1af26983747da131ebef04
Kai Wang <charles.wangkai@gmail.com>
2022-08-19 19:49:35 +0800

    Interesting Sum

---
+++ 1720/B/Main.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int[] a) {
+    Arrays.sort(a);
+
+    return a[a.length - 1] + a[a.length - 2] - a[0] - a[1];
+  }
+}
