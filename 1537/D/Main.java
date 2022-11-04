6007d1d38d2ba594dcd82b6bded444036bcbb2bd
Kai Wang <charles.wangkai@gmail.com>
2022-11-04 20:45:07 +0800

    Deleting Divisors

---
+++ 1537/D/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+
+      System.out.println(solve(n));
+    }
+
+    sc.close();
+  }
+
+  static String solve(int n) {
+    return (n % 2 == 0 && (Integer.bitCount(n) != 1 || Integer.toBinaryString(n).length() % 2 != 0))
+        ? "Alice"
+        : "Bob";
+  }
+}
