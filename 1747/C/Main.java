fbc32a0dad07416e8e8a8d88c6dfb1834e64e766
Kai Wang <charles.wangkai@gmail.com>
2022-11-08 18:06:54 +0800

    Swap Game

---
+++ 1747/C/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a));
+    }
+
+    sc.close();
+  }
+
+  static String solve(int[] a) {
+    return IntStream.range(1, a.length).anyMatch(i -> a[i] <= a[0] - 1) ? "Alice" : "Bob";
+  }
+}
