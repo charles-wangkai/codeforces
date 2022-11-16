c0e8f0fd48d002b3ddea171d463b5ba4fbdfa41c
Kai Wang <charles.wangkai@gmail.com>
2022-11-16 21:13:08 +0800

    Maximum Crossings (Easy Version)

---
+++ 1676/H1/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int[] a) {
+    return IntStream.range(0, a.length)
+        .map(i -> (int) IntStream.range(i + 1, a.length).filter(j -> a[j] <= a[i]).count())
+        .sum();
+  }
+}
