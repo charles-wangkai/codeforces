d62a7512b1558a6cfb5b02db330155d5b1f9f6f4
Kai Wang <charles.wangkai@gmail.com>
2022-11-29 08:26:35 +0800

    Doremy's IQ

--- 1707/A/Main.java
+++ 1707/A/Main.java
@@ -1,3 +1,5 @@
+// https://codeforces.com/blog/entry/104930
+
 import java.util.Scanner;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -22,69 +24,19 @@ public class Main {
   }
 
   static String solve(int[] a, int q) {
-    int[] sortedIndices =
-        IntStream.range(0, a.length)
-            .boxed()
-            .sorted(
-                (i1, i2) -> {
-                  boolean le1 = a[i1] <= q;
-                  boolean le2 = a[i2] <= q;
-                  int indexCmp = Integer.compare(i1, i2);
-                  if (le1) {
-                    if (le2) {
-                      return indexCmp;
-                    } else {
-                      return -1;
-                    }
-                  } else {
-                    if (le2) {
-                      return 1;
-                    } else {
-                      return -indexCmp;
-                    }
-                  }
-                })
-            .mapToInt(x -> x)
-            .toArray();
-
-    String result = "0".repeat(a.length);
-    int lower = 1;
-    int upper = a.length;
-    while (lower <= upper) {
-      int middle = (lower + upper) / 2;
-
-      boolean[] chosen = new boolean[a.length];
-      for (int i = 0; i < middle; ++i) {
-        chosen[sortedIndices[i]] = true;
-      }
-
-      if (check(a, q, chosen)) {
-        result =
-            IntStream.range(0, chosen.length)
-                .mapToObj(i -> chosen[i] ? "1" : "0")
-                .collect(Collectors.joining());
-        lower = middle + 1;
-      } else {
-        upper = middle - 1;
+    boolean[] chosen = new boolean[a.length];
+    int current = 0;
+    for (int i = chosen.length - 1; i >= 0; --i) {
+      if (a[i] <= current) {
+        chosen[i] = true;
+      } else if (current != q) {
+        chosen[i] = true;
+        ++current;
       }
     }
 
-    return result;
-  }
-
-  static boolean check(int[] a, int q, boolean[] chosen) {
-    for (int i = 0; i < a.length; ++i) {
-      if (chosen[i]) {
-        if (q == 0) {
-          return false;
-        }
-
-        if (a[i] > q) {
-          --q;
-        }
-      }
-    }
-
-    return true;
+    return IntStream.range(0, chosen.length)
+        .mapToObj(i -> chosen[i] ? "1" : "0")
+        .collect(Collectors.joining());
   }
 }
