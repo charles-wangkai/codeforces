1d4c6c96411145781582c3151ab6376493870f21
Kai Wang <charles.wangkai@gmail.com>
2022-12-31 13:54:00 +0800

    Joey Takes Money

---
+++ 1731/A/Main.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a));
+    }
+
+    sc.close();
+  }
+
+  static long solve(int[] a) {
+    return (Arrays.stream(a).asLongStream().reduce((x, y) -> x * y).getAsLong() + a.length - 1)
+        * 2022;
+  }
+}
