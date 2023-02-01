6c9b894efbaf9cb2a8047bec5478dc783131a65a
Kai Wang <charles.wangkai@gmail.com>
2023-02-02 03:43:02 +0800

    Flip Flop Sum

---
+++ 1778/A/Main.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int[] a) {
+    int sum = Arrays.stream(a).sum();
+
+    return IntStream.range(0, a.length - 1).anyMatch(i -> a[i] == -1 && a[i + 1] == -1)
+        ? (sum + 4)
+        : (IntStream.range(0, a.length - 1).anyMatch(i -> a[i] == -a[i + 1]) ? sum : (sum - 4));
+  }
+}
