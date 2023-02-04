cc13a536f2551f0451fd1615e4774cf16d4180d1
Kai Wang <charles.wangkai@gmail.com>
2023-02-04 23:56:11 +0800

    Negatives and Positives

---
+++ 1791/E/Main.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a));
+    }
+
+    sc.close();
+  }
+
+  static long solve(int[] a) {
+    return Arrays.stream(a).map(Math::abs).asLongStream().sum()
+        - ((Arrays.stream(a).filter(x -> x < 0).count() % 2 == 0)
+            ? 0
+            : 2 * Arrays.stream(a).map(Math::abs).min().getAsInt());
+  }
+}
