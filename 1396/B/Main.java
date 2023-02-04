63a6125cf76a6a3ff4aed587d62f4d89b413e7a0
Kai Wang <charles.wangkai@gmail.com>
2023-02-04 16:23:01 +0800

    Stoned Game

---
+++ 1396/B/Main.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a));
+    }
+
+    sc.close();
+  }
+
+  static String solve(int[] a) {
+    return (Arrays.stream(a).sum() % 2 == 1
+            || Arrays.stream(a).sum() < Arrays.stream(a).max().getAsInt() * 2)
+        ? "T"
+        : "HL";
+  }
+}
