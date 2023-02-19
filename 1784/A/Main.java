b453b682a48e53ba47cb2420e8c23ea0af1c6ef5
Kai Wang <charles.wangkai@gmail.com>
2023-02-19 10:14:26 +0800

    Monsters (easy version)

---
+++ 1784/A/Main.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a));
+    }
+
+    sc.close();
+  }
+
+  static long solve(int[] a) {
+    Arrays.sort(a);
+
+    long result = 0;
+    int needed = 1;
+    for (int ai : a) {
+      if (ai >= needed) {
+        result += ai - needed;
+        ++needed;
+      }
+    }
+
+    return result;
+  }
+}
