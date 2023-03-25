21938a87867faee77f7953eed9b6ebb7a6a6da21
Kai Wang <charles.wangkai@gmail.com>
2023-03-25 15:10:42 +0800

    Super Agent

---
+++ 12/A/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+  static final int SIZE = 3;
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    char[][] matrix = new char[SIZE][SIZE];
+    for (int r = 0; r < SIZE; ++r) {
+      String line = sc.next();
+      for (int c = 0; c < SIZE; ++c) {
+        matrix[r][c] = line.charAt(c);
+      }
+    }
+
+    System.out.println(solve(matrix) ? "YES" : "NO");
+
+    sc.close();
+  }
+
+  static boolean solve(char[][] matrix) {
+    for (int r = 0; r < SIZE; ++r) {
+      for (int c = 0; c < SIZE; ++c) {
+        if (matrix[r][c] != matrix[2 - r][2 - c]) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+}
