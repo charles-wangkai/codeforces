c9bf31784f0a8daefe601fd971a4d612442b783c
Kai Wang <charles.wangkai@gmail.com>
2023-04-25 23:07:57 +0800

    Bun Lover

---
+++ 1822/C/Main.java
@@ -0,0 +1,22 @@
+// https://oeis.org/A002522
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+
+      System.out.println(solve(n));
+    }
+
+    sc.close();
+  }
+
+  static long solve(int n) {
+    return (n + 1L) * (n + 1) + 1;
+  }
+}
