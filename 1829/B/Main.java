8381598a289ba0312299a4f08b98232b2d96f5d1
Kai Wang <charles.wangkai@gmail.com>
2023-05-07 10:52:52 +0800

    Blank Space

---
+++ 1829/B/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int[] a) {
+    int result = 0;
+    int zeroCount = 0;
+    for (int ai : a) {
+      if (ai == 0) {
+        ++zeroCount;
+        result = Math.max(result, zeroCount);
+      } else {
+        zeroCount = 0;
+      }
+    }
+
+    return result;
+  }
+}
