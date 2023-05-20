54872da214dbb225a8d4aedfc3dbfd92a3a3b110
Kai Wang <charles.wangkai@gmail.com>
2023-05-20 08:49:46 +0800

    Musical Puzzle

---
+++ 1833/A/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      sc.nextInt();
+      String s = sc.next();
+
+      System.out.println(solve(s));
+    }
+
+    sc.close();
+  }
+
+  static int solve(String s) {
+    return (int)
+        IntStream.range(0, s.length() - 1).mapToObj(i -> s.substring(i, i + 2)).distinct().count();
+  }
+}
