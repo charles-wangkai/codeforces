6011513b21eb7f35bfca67fa53730132badcf49a
Kai Wang <charles.wangkai@gmail.com>
2023-05-30 04:28:37 +0800

    Twin Permutations

---
+++ 1831/A/Main.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a));
+    }
+
+    sc.close();
+  }
+
+  static String solve(int[] a) {
+    return Arrays.stream(a)
+        .map(x -> a.length + 1 - x)
+        .mapToObj(String::valueOf)
+        .collect(Collectors.joining(" "));
+  }
+}
