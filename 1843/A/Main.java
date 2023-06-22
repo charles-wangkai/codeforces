b5d8c205024d53c8f1c3a4edd0ce8682286fb7cd
Kai Wang <charles.wangkai@gmail.com>
2023-06-22 09:49:34 +0800

    Sasha and Array Coloring

---
+++ 1843/A/Main.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int[] a) {
+    Arrays.sort(a);
+
+    int result = 0;
+    for (int i = 0, j = a.length - 1; i < j; ++i, --j) {
+      result += a[j] - a[i];
+    }
+
+    return result;
+  }
+}
