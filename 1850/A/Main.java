f18603c612677692c38eb6dae3e5247e12341c9b
Kai Wang <charles.wangkai@gmail.com>
2023-07-22 14:47:54 +0800

    To My Critics

---
+++ 1850/A/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      int c = sc.nextInt();
+
+      System.out.println(solve(a, b, c) ? "YES" : "NO");
+    }
+
+    sc.close();
+  }
+
+  static boolean solve(int a, int b, int c) {
+    return IntStream.of(a, b, c).sorted().skip(1).sum() >= 10;
+  }
+}
