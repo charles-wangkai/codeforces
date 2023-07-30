d929455939f9c0215e2c60b181e039c7b154dca0
Kai Wang <charles.wangkai@gmail.com>
2023-07-30 15:24:58 +0800

    Dalton the Teacher

---
+++ 1855/A/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] p = new int[n];
+      for (int i = 0; i < p.length; ++i) {
+        p[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(p));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int[] p) {
+    return ((int) IntStream.range(0, p.length).filter(i -> p[i] == i + 1).count() + 1) / 2;
+  }
+}
