15e632c921878777e6494d756365e4ea3d78290a
Kai Wang <charles.wangkai@gmail.com>
2023-09-25 11:58:34 +0800

    Chips on the Board

---
+++ 1879/B/Main.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+      int[] b = new int[n];
+      for (int i = 0; i < b.length; ++i) {
+        b[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a, b));
+    }
+
+    sc.close();
+  }
+
+  static long solve(int[] a, int[] b) {
+    return Math.min(
+        Arrays.stream(a).asLongStream().sum() + (long) Arrays.stream(b).min().getAsInt() * a.length,
+        Arrays.stream(b).asLongStream().sum()
+            + (long) Arrays.stream(a).min().getAsInt() * a.length);
+  }
+}
