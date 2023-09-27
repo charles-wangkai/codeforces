27a47e9e5af00c178fbb80f182b3be0dc9b6004a
Kai Wang <charles.wangkai@gmail.com>
2023-09-27 08:23:39 +0800

    How Much Does Daytona Cost?

---
+++ 1878/A/Main.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int k = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a, k) ? "YES" : "NO");
+    }
+
+    sc.close();
+  }
+
+  static boolean solve(int[] a, int k) {
+    return Arrays.stream(a).anyMatch(x -> x == k);
+  }
+}
