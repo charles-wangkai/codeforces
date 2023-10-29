4c7ca88216d4e1108495742903dfeb157b9d1fac
Kai Wang <charles.wangkai@gmail.com>
2023-10-29 22:55:47 +0800

    Doremy's Paint 3

---
+++ 1890/A/Main.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a) ? "Yes" : "No");
+    }
+
+    sc.close();
+  }
+
+  static boolean solve(int[] a) {
+    Map<Integer, Integer> valueToCount = new HashMap<>();
+    for (int ai : a) {
+      valueToCount.put(ai, valueToCount.getOrDefault(ai, 0) + 1);
+    }
+
+    int[] counts = valueToCount.values().stream().mapToInt(Integer::intValue).toArray();
+
+    return counts.length == 1 || (counts.length == 2 && Math.abs(counts[0] - counts[1]) <= 1);
+  }
+}
