486c8440f2ac2c654dd2c35ed83f385caae6e7e5
Kai Wang <charles.wangkai@gmail.com>
2023-12-02 23:38:54 +0800

    Repeating Cipher

---
+++ 1911/B.kt
@@ -0,0 +1,22 @@
+import java.util.Scanner
+
+fun main(args: Array<String>) {
+    val sc = Scanner(System.`in`)
+    sc.nextInt()
+    val s = sc.next()
+    println(solve(s))
+    sc.close()
+}
+
+fun solve(s: String): String {
+    val result = StringBuilder()
+    var index = 0
+    var step = 1
+    while (index < s.length) {
+        result.append(s[index])
+        index += step
+        step++
+    }
+    return result.toString()
+}
+
