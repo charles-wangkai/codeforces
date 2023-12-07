a16a770bfcac38b4164b02118784d23fcef9e5c0
Kai Wang <charles.wangkai@gmail.com>
2023-12-07 08:55:57 +0800

    Removal of Unattractive Pairs

---
+++ 1907/C/Main.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      sc.nextInt();
+      String s = sc.next();
+
+      System.out.println(solve(s));
+    }
+
+    sc.close();
+  }
+
+  static int solve(String s) {
+    Map<Character, Integer> letterToCount = new HashMap<>();
+    for (char letter : s.toCharArray()) {
+      letterToCount.put(letter, letterToCount.getOrDefault(letter, 0) + 1);
+    }
+
+    int maxCount = letterToCount.values().stream().mapToInt(Integer::intValue).max().getAsInt();
+    int otherCount = s.length() - maxCount;
+
+    return (maxCount < otherCount) ? ((otherCount - maxCount) % 2) : (maxCount - otherCount);
+  }
+}
