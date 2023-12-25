15b37e8459eb6226bbb460f85fcaaa41a0c4d5ca
Kai Wang <charles.wangkai@gmail.com>
2023-12-25 08:37:31 +0800

    Least Product

---
+++ 1917/A/Main.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a));
+    }
+
+    sc.close();
+  }
+
+  static String solve(int[] a) {
+    return (Arrays.stream(a).anyMatch(x -> x == 0)
+            || Arrays.stream(a).filter(x -> x < 0).count() % 2 == 1)
+        ? "0"
+        : "1\n1 0";
+  }
+}
