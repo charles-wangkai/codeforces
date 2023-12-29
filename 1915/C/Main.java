275e54c634b6b296533c76f61c3ef119afc9086c
Kai Wang <charles.wangkai@gmail.com>
2023-12-29 08:33:15 +0800

    Can I Square?

---
+++ 1915/C/Main.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a) ? "YES" : "NO");
+    }
+
+    sc.close();
+  }
+
+  static boolean solve(int[] a) {
+    long sum = Arrays.stream(a).asLongStream().sum();
+    int root = (int) Math.round(Math.sqrt(sum));
+
+    return (long) root * root == sum;
+  }
+}
