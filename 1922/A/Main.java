ac5b8a148c8fa9e8a8de5e84c6d9629af9424f9d
Kai Wang <charles.wangkai@gmail.com>
2024-01-19 05:38:57 +0800

    Tricky Template

---
+++ 1922/A/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      sc.nextInt();
+      String a = sc.next();
+      String b = sc.next();
+      String c = sc.next();
+
+      System.out.println(solve(a, b, c) ? "YES" : "NO");
+    }
+
+    sc.close();
+  }
+
+  static boolean solve(String a, String b, String c) {
+    return IntStream.range(0, a.length())
+        .anyMatch(i -> c.charAt(i) != a.charAt(i) && c.charAt(i) != b.charAt(i));
+  }
+}
