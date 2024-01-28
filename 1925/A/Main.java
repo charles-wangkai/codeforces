f505f6828cb29fc259a3b05e8fd9bd909d39f7c3
Kai Wang <charles.wangkai@gmail.com>
2024-01-28 09:01:03 +0800

    We Got Everything Covered!

---
+++ 1925/A/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int k = sc.nextInt();
+
+      System.out.println(solve(n, k));
+    }
+
+    sc.close();
+  }
+
+  static String solve(int n, int k) {
+    return IntStream.range(0, k)
+        .mapToObj(i -> (char) (i + 'a'))
+        .map(String::valueOf)
+        .collect(Collectors.joining())
+        .repeat(n);
+  }
+}
