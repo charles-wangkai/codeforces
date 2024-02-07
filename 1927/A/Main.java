2c2acd63d768cca4aec6b3681e6dde5c1b5ab5cd
Kai Wang <charles.wangkai@gmail.com>
2024-02-07 08:10:57 +0800

    Make it White

---
+++ 1927/A/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      sc.nextInt();
+      String s = sc.next();
+
+      System.out.println(solve(s));
+    }
+
+    sc.close();
+  }
+
+  static int solve(String s) {
+    return s.lastIndexOf('B') - s.indexOf('B') + 1;
+  }
+}
