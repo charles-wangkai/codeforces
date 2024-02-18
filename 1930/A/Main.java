3cb1c2f820c71eae569a57bed6a9c2259ab844ca
Kai Wang <charles.wangkai@gmail.com>
2024-02-18 09:01:20 +0800

    Maximise The Score

---
+++ 1930/A/Main.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[2 * n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int[] a) {
+    Arrays.sort(a);
+
+    return IntStream.range(0, a.length / 2).map(i -> a[2 * i]).sum();
+  }
+}
