b1b2d6ac8890d68584724ce757b9836e5e27f579
Kai Wang <charles.wangkai@gmail.com>
2024-03-19 21:31:29 +0800

    Setting up Camp

---
+++ 1945/A/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      int c = sc.nextInt();
+
+      System.out.println(solve(a, b, c));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int a, int b, int c) {
+    return (c < (3 - b % 3) % 3) ? -1 : (a + (b + c + 2) / 3);
+  }
+}
