dae8fdce40d9779eb14019ba5015899f34dbbf69
Kai Wang <charles.wangkai@gmail.com>
2024-04-13 09:01:55 +0800

    Make It Ugly

---
+++ 1954/B/Main.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int[] a) {
+    if (Arrays.stream(a).distinct().count() == 1) {
+      return -1;
+    }
+
+    int result = Integer.MAX_VALUE;
+    int count = 0;
+    for (int i = 0; i <= a.length; ++i) {
+      if (i != a.length && a[i] == a[0]) {
+        ++count;
+      } else {
+        result = Math.min(result, count);
+        count = 0;
+      }
+    }
+
+    return result;
+  }
+}
