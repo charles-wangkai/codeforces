563f87181f1e9b29addfcd3af0ac35c6a9c6aea4
Kai Wang <charles.wangkai@gmail.com>
2024-05-31 08:30:32 +0800

    Verify Password

---
+++ 1976/A/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      sc.nextInt();
+      String s = sc.next();
+
+      System.out.println(solve(s) ? "YES" : "NO");
+    }
+
+    sc.close();
+  }
+
+  static boolean solve(String s) {
+    return IntStream.range(0, s.length() - 1).allMatch(i -> s.charAt(i) <= s.charAt(i + 1));
+  }
+}
