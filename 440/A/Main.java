88f8d917c5840f5650f17f787aaea92b787fbe61
Kai Wang <charles.wangkai@gmail.com>
2024-06-07 20:15:43 +0800

    Forgotten Episode

---
+++ 440/A/Main.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+    int[] a = new int[n - 1];
+    for (int i = 0; i < a.length; ++i) {
+      a[i] = sc.nextInt();
+    }
+
+    System.out.println(solve(a));
+
+    sc.close();
+  }
+
+  static int solve(int[] a) {
+    int n = a.length + 1;
+
+    return (int) (n * (n + 1L) / 2 - Arrays.stream(a).asLongStream().sum());
+  }
+}
