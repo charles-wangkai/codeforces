31a9c590c7ddbd4420615a3d9655eeb87fa024db
Kai Wang <charles.wangkai@gmail.com>
2024-07-04 08:30:16 +0800

    Winnie-the-Pooh and honey

---
+++ 120/C/Main.java
@@ -0,0 +1,26 @@
+import java.io.File;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) throws Throwable {
+    Scanner sc = new Scanner(new File("input.txt"));
+    PrintStream out = new PrintStream("output.txt");
+
+    int n = sc.nextInt();
+    int k = sc.nextInt();
+    int[] a = new int[n];
+    for (int i = 0; i < a.length; ++i) {
+      a[i] = sc.nextInt();
+    }
+
+    out.println(solve(a, k));
+
+    sc.close();
+  }
+
+  static int solve(int[] a, int k) {
+    return Arrays.stream(a).map(ai -> ai - Math.min(3, ai / k) * k).sum();
+  }
+}
