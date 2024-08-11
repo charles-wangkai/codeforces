2c7a8f8fc1efeb9400c6ffaf629e8ddfff322744
Kai Wang <charles.wangkai@gmail.com>
2024-08-12 01:27:29 +0800

    Minimize Equal Sum Subarrays

---
+++ 1998/B/Main.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] p = new int[n];
+      for (int i = 0; i < p.length; ++i) {
+        p[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(p));
+    }
+
+    sc.close();
+  }
+
+  static String solve(int[] p) {
+    return Arrays.stream(p)
+        .map(pi -> (pi == p.length) ? 1 : (pi + 1))
+        .mapToObj(String::valueOf)
+        .collect(Collectors.joining(" "));
+  }
+}
