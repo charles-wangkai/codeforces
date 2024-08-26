82399f77e8bd3e5ca1cf08df5e2b3356ea376dd4
Kai Wang <charles.wangkai@gmail.com>
2024-08-26 17:06:11 +0800

    Turtle and Good Strings

---
+++ 2003/A/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      sc.nextInt();
+      String s = sc.next();
+
+      System.out.println(solve(s) ? "YES" : "NO");
+    }
+
+    sc.close();
+  }
+
+  static boolean solve(String s) {
+    return s.charAt(0) != s.charAt(s.length() - 1);
+  }
+}
