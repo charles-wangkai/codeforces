e56624cd73c25d532a4f4c127ce65642e7e8b85b
Kai Wang <charles.wangkai@gmail.com>
2024-09-07 18:23:57 +0800

    Sakurako's Box

---
+++ 2008/F/Main.java
@@ -0,0 +1,45 @@
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+  static final int MODULUS = 1_000_000_007;
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int[] a) {
+    int sum = Arrays.stream(a).reduce(Main::addMod).getAsInt();
+
+    return divideMod(
+        multiplyMod(sum, sum)
+            - Arrays.stream(a).map(ai -> multiplyMod(ai, ai)).reduce(Main::addMod).getAsInt(),
+        multiplyMod(a.length, a.length - 1));
+  }
+
+  static int addMod(int x, int y) {
+    return Math.floorMod(x + y, MODULUS);
+  }
+
+  static int multiplyMod(int x, int y) {
+    return Math.floorMod((long) x * y, MODULUS);
+  }
+
+  static int divideMod(int x, int y) {
+    return multiplyMod(x, BigInteger.valueOf(y).modInverse(BigInteger.valueOf(MODULUS)).intValue());
+  }
+}
