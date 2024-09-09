0ef729eced585e55d7a011adb565d24df78217af
Kai Wang <charles.wangkai@gmail.com>
2024-09-10 07:14:08 +0800

    Three Brothers

---
+++ 2010/B/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+    System.out.println(solve(a, b));
+
+    sc.close();
+  }
+
+  static int solve(int a, int b) {
+    return 6 - a - b;
+  }
+}
