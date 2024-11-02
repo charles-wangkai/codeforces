dccf9c552ae63b51078a50613451b6121fd99e4d
Kai Wang <charles.wangkai@gmail.com>
2024-11-02 17:54:30 +0800

    Circuit

---
+++ 2032/A/Main.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[2 * n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a));
+    }
+
+    sc.close();
+  }
+
+  static String solve(int[] a) {
+    int zeroCount = (int) Arrays.stream(a).filter(ai -> ai == 0).count();
+
+    return "%d %d".formatted(zeroCount % 2, Math.min(zeroCount, a.length - zeroCount));
+  }
+}
