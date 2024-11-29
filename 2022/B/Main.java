d28281b1b080b8c87987f991456bc2c1aa19a44d
Kai Wang <charles.wangkai@gmail.com>
2024-11-29 15:07:24 +0800

    Kar Salesman

---
+++ 2022/B/Main.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws Throwable {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int t = Integer.parseInt(st.nextToken());
+    for (int tc = 0; tc < t; ++tc) {
+      st = new StringTokenizer(br.readLine());
+      int n = Integer.parseInt(st.nextToken());
+      int x = Integer.parseInt(st.nextToken());
+      int[] a = new int[n];
+      st = new StringTokenizer(br.readLine());
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = Integer.parseInt(st.nextToken());
+      }
+
+      System.out.println(solve(a, x));
+    }
+  }
+
+  static long solve(int[] a, int x) {
+    return Math.max(
+        Arrays.stream(a).max().getAsInt(), (Arrays.stream(a).asLongStream().sum() + x - 1) / x);
+  }
+}
