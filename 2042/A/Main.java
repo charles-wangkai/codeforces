03ec992fbeee1387fafd1f172f0e356cd08f4a06
Kai Wang <charles.wangkai@gmail.com>
2024-12-03 08:07:48 +0800

    Greedy Monocarp

---
+++ 2042/A/Main.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int k = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a, k));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int[] a, int k) {
+    int[] sorted =
+        Arrays.stream(a)
+            .boxed()
+            .sorted(Comparator.reverseOrder())
+            .mapToInt(Integer::intValue)
+            .toArray();
+
+    int sum = 0;
+    for (int i = 0; i < sorted.length && sum + sorted[i] <= k; ++i) {
+      sum += sorted[i];
+    }
+
+    return k - sum;
+  }
+}
