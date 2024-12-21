1896fbdd8ffb139378a28119435deaf01681b4bd
Kai Wang <charles.wangkai@gmail.com>
2024-12-21 10:15:37 +0800

    MEX Destruction

---
+++ 2049/A/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int[] a) {
+    return Math.min(
+        2,
+        (int)
+            IntStream.range(0, a.length)
+                .filter(i -> a[i] != 0 && (i == 0 || a[i - 1] == 0))
+                .count());
+  }
+}
