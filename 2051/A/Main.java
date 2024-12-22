2cb9f0bbcfdc1d3322f405fc4ce6785f76b77725
Kai Wang <charles.wangkai@gmail.com>
2024-12-23 07:16:54 +0800

    Preparing for the Olympiad

---
+++ 2051/A/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+      int[] b = new int[n];
+      for (int i = 0; i < b.length; ++i) {
+        b[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a, b));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int[] a, int[] b) {
+    return IntStream.range(0, a.length)
+        .map(i -> Math.max(0, a[i] - ((i == a.length - 1) ? 0 : b[i + 1])))
+        .sum();
+  }
+}
