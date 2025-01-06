9194c1f892bb9e54daf5c10b0e581e4eee6489d3
Kai Wang <charles.wangkai@gmail.com>
2025-01-06 09:17:35 +0800

    Gorilla and the Exam

---
+++ 2057/B/Main.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int k = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a, k));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int[] a, int k) {
+    Map<Integer, Integer> valueToCount = new HashMap<>();
+    for (int ai : a) {
+      valueToCount.put(ai, valueToCount.getOrDefault(ai, 0) + 1);
+    }
+
+    int[] sortedCounts =
+        valueToCount.values().stream().mapToInt(Integer::intValue).sorted().toArray();
+
+    int result = valueToCount.size();
+    for (int count : sortedCounts) {
+      if (k >= count && result != 1) {
+        k -= count;
+        --result;
+      }
+    }
+
+    return result;
+  }
+}
