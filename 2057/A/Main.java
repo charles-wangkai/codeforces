9219fffdb0aa679278266abc50d93e9b6a5257b8
Kai Wang <charles.wangkai@gmail.com>
2025-01-06 08:45:23 +0800

    MEX Table

---
+++ 2057/A/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int m = sc.nextInt();
+
+      System.out.println(solve(n, m));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int n, int m) {
+    if (n < m) {
+      return solve(m, n);
+    }
+
+    return n + 1;
+  }
+}
