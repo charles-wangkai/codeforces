7e9b8a90e768cc66cbcda4c843c2ddfa746bd1dd
Kai Wang <charles.wangkai@gmail.com>
2025-02-03 20:28:49 +0800

    Milya and Two Arrays

---
+++ 2059/A/Main.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+      int[] b = new int[n];
+      for (int i = 0; i < b.length; ++i) {
+        b[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a, b) ? "YES" : "NO");
+    }
+
+    sc.close();
+  }
+
+  static boolean solve(int[] a, int[] b) {
+    return Arrays.stream(a).distinct().count() >= 3
+        || Arrays.stream(b).distinct().count() >= 3
+        || (Arrays.stream(a).distinct().count() == 2 && Arrays.stream(b).distinct().count() == 2);
+  }
+}
