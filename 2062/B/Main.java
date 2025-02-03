fa552331de22300da9dc7cffb55d9928aed93250
Kai Wang <charles.wangkai@gmail.com>
2025-02-03 22:32:57 +0800

    Clockwork

---
+++ 2062/B/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a) ? "YES" : "NO");
+    }
+
+    sc.close();
+  }
+
+  static boolean solve(int[] a) {
+    return IntStream.range(0, a.length)
+        .allMatch(i -> a[i] > Math.max(1, Math.max(i, a.length - 1 - i) * 2));
+  }
+}
