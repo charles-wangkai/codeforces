554617c843f59f1e692ad9dea949d1f5e33725a8
Kai Wang <charles.wangkai@gmail.com>
2025-04-24 09:23:41 +0800

    Tiling with Hexagons

---
+++ 216/A/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int c = sc.nextInt();
+
+    System.out.println(solve(a, b, c));
+
+    sc.close();
+  }
+
+  static int solve(int a, int b, int c) {
+    return b * c + (a - 1) * (b + c - 1);
+  }
+}
