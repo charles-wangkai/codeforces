a008432d4581a0e59136702a9c3218c7f2d0ad9e
Kai Wang <charles.wangkai@gmail.com>
2025-06-18 17:15:43 +0800

    Selection of Personnel

---
+++ 630/F/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+
+    System.out.println(solve(n));
+
+    sc.close();
+  }
+
+  static long solve(int n) {
+    return C(n, 5) + C(n, 6) + C(n, 7);
+  }
+
+  static long C(int n, int r) {
+    long result = 1;
+    for (int i = 0; i < r; ++i) {
+      result = result * (n - i) / (i + 1);
+    }
+
+    return result;
+  }
+}
