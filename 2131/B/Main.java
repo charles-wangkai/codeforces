fa40cfb0e27c69ecb2e548af30d18b40f33d035f
Kai Wang <charles.wangkai@gmail.com>
2025-08-11 09:12:05 +0800

    Alternating Series

---
+++ 2131/B/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+
+      System.out.println(solve(n));
+    }
+
+    sc.close();
+  }
+
+  static String solve(int n) {
+    return IntStream.range(0, n)
+        .map(i -> (i % 2 == 0) ? -1 : ((i == n - 1) ? 2 : 3))
+        .mapToObj(String::valueOf)
+        .collect(Collectors.joining(" "));
+  }
+}
