ff646a174d812bb79b0da3e838c905b0e6c9e031
Kai Wang <charles.wangkai@gmail.com>
2025-08-27 12:12:38 +0800

    Add 0 or K

---
+++ 2134/B/Main.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int k = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a, k));
+    }
+
+    sc.close();
+  }
+
+  static String solve(int[] a, int k) {
+    return Arrays.stream(a)
+        .mapToLong(ai -> ai + ai % (k + 1L) * k)
+        .mapToObj(String::valueOf)
+        .collect(Collectors.joining(" "));
+  }
+}
