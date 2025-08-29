abb65c35c98ffb8dd9920ec3b55910e0052bd95d
Kai Wang <charles.wangkai@gmail.com>
2025-08-29 22:33:21 +0800

    In the Dream

---
+++ 2136/A/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      int c = sc.nextInt();
+      int d = sc.nextInt();
+
+      System.out.println(solve(a, b, c, d) ? "YES" : "NO");
+    }
+
+    sc.close();
+  }
+
+  static boolean solve(int a, int b, int c, int d) {
+    return check(a, b) && check(c - a, d - b);
+  }
+
+  static boolean check(int x, int y) {
+    if (x > y) {
+      return check(y, x);
+    }
+
+    return y <= x * 2 + 2;
+  }
+}
