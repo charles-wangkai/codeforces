fbaa0c1af1a50b035bdea8ff009d25a88eaaa761
Kai Wang <charles.wangkai@gmail.com>
2025-09-10 12:19:37 +0800

    Shift Sort

---
+++ 2140/A/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      sc.nextInt();
+      String s = sc.next();
+
+      System.out.println(solve(s));
+    }
+
+    sc.close();
+  }
+
+  static int solve(String s) {
+    return s.substring(0, s.replace("1", "").length()).replace("0", "").length();
+  }
+}
