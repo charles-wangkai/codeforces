50906cbb7fb118da65271083110542a666ece008
Kai Wang <charles.wangkai@gmail.com>
2025-09-14 07:55:30 +0800

    Lasers

---
+++ 2148/B/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int m = sc.nextInt();
+      int x = sc.nextInt();
+      int y = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+      int[] b = new int[m];
+      for (int i = 0; i < b.length; ++i) {
+        b[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a, b, x, y));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int[] a, int[] b, int x, int y) {
+    return a.length + b.length;
+  }
+}
