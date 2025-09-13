dc3eee24d421fd4f92d72f669d346c8a03d48950
Kai Wang <charles.wangkai@gmail.com>
2025-09-14 07:47:38 +0800

    Sublime Sequence

---
+++ 2148/A/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int x = sc.nextInt();
+      int n = sc.nextInt();
+
+      System.out.println(solve(x, n));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int x, int n) {
+    return (n % 2 == 0) ? 0 : x;
+  }
+}
