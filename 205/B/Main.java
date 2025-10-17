086d77e36285f0b8f154dafaafe8484dab66a5a6
Kai Wang <charles.wangkai@gmail.com>
2025-10-17 17:05:05 +0800

    Little Elephant and Sorting

---
+++ 205/B/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+    int[] a = new int[n];
+    for (int i = 0; i < a.length; ++i) {
+      a[i] = sc.nextInt();
+    }
+
+    System.out.println(solve(a));
+
+    sc.close();
+  }
+
+  static long solve(int[] a) {
+    return IntStream.range(0, a.length - 1)
+        .map(i -> Math.max(0, a[i] - a[i + 1]))
+        .asLongStream()
+        .sum();
+  }
+}
