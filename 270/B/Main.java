9c9a2fd949e4ea06fb3c85ef727e3a499a99c4fb
Kai Wang <charles.wangkai@gmail.com>
2025-11-03 19:33:50 +0800

    Multithreading

---
+++ 270/B/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+    int[] a = new int[n];
+    for (int i = 0; i < a.length; ++i) {
+      a[i] = sc.nextInt();
+    }
+
+    System.out.println(solve(a));
+
+    sc.close();
+  }
+
+  static int solve(int[] a) {
+    int result = a.length - 1;
+    while (result != 0 && a[result] > a[result - 1]) {
+      --result;
+    }
+
+    return result;
+  }
+}
