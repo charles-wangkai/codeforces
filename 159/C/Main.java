d65f4a70ab448d7a6c2972d4317bf439a3fd537d
Kai Wang <charles.wangkai@gmail.com>
2025-11-19 15:35:20 +0800

    String Manipulation 1.0

---
+++ 159/C/Main.java
@@ -0,0 +1,93 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int k = sc.nextInt();
+    String s = sc.next();
+    int n = sc.nextInt();
+    int[] p = new int[n];
+    char[] c = new char[n];
+    for (int i = 0; i < n; ++i) {
+      p[i] = sc.nextInt();
+      c[i] = sc.next().charAt(0);
+    }
+
+    System.out.println(solve(k, s, p, c));
+
+    sc.close();
+  }
+
+  static String solve(int k, String s, int[] p, char[] c) {
+    String t = s.repeat(k);
+
+    Map<Character, List<Integer>> letterToIndices = new HashMap<>();
+    for (int i = 0; i < t.length(); ++i) {
+      letterToIndices.putIfAbsent(t.charAt(i), new ArrayList<>());
+      letterToIndices.get(t.charAt(i)).add(i);
+    }
+
+    Map<Character, int[]> letterToBinaryIndexedTree = new HashMap<>();
+    for (char letter : letterToIndices.keySet()) {
+      letterToBinaryIndexedTree.put(
+          letter, new int[Integer.highestOneBit(letterToIndices.get(letter).size()) * 2 + 1]);
+    }
+
+    boolean[] deleted = new boolean[t.length()];
+    for (int i = 0; i < p.length; ++i) {
+      List<Integer> indices = letterToIndices.get(c[i]);
+      int[] binaryIndexedTree = letterToBinaryIndexedTree.get(c[i]);
+
+      int pos = find(binaryIndexedTree, indices.size(), p[i] - 1);
+      deleted[indices.get(pos)] = true;
+      add(binaryIndexedTree, pos + 1, 1);
+    }
+
+    return IntStream.range(0, deleted.length)
+        .filter(i -> !deleted[i])
+        .mapToObj(t::charAt)
+        .map(String::valueOf)
+        .collect(Collectors.joining());
+  }
+
+  static int find(int[] binaryIndexedTree, int size, int index) {
+    int result = -1;
+    int lower = 0;
+    int upper = size - 1;
+    while (lower <= upper) {
+      int middle = (lower + upper) / 2;
+      if (middle - computeSum(binaryIndexedTree, middle) <= index) {
+        result = middle;
+        lower = middle + 1;
+      } else {
+        upper = middle - 1;
+      }
+    }
+
+    return result;
+  }
+
+  static void add(int[] binaryIndexedTree, int i, int x) {
+    while (i < binaryIndexedTree.length) {
+      binaryIndexedTree[i] += x;
+      i += i & -i;
+    }
+  }
+
+  static int computeSum(int[] binaryIndexedTree, int i) {
+    int result = 0;
+    while (i != 0) {
+      result += binaryIndexedTree[i];
+      i -= i & -i;
+    }
+
+    return result;
+  }
+}
