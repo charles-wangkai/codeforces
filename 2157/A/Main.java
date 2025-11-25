40007439b0adaa2239e982461509c907a11f2848
Kai Wang <charles.wangkai@gmail.com>
2025-11-25 09:44:01 +0800

    Dungeon Equilibrium

---
+++ 2157/A/Main.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int[] a) {
+    Map<Integer, Integer> valueToCount = new HashMap<>();
+    for (int ai : a) {
+      valueToCount.put(ai, valueToCount.getOrDefault(ai, 0) + 1);
+    }
+
+    return valueToCount.keySet().stream()
+        .mapToInt(
+            value ->
+                (value == 0 || valueToCount.get(value) < value)
+                    ? valueToCount.get(value)
+                    : (valueToCount.get(value) - value))
+        .sum();
+  }
+}
