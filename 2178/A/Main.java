710638b75236827e4d784173ac22666c10922a14
Kai Wang <charles.wangkai@gmail.com>
2025-12-29 08:45:29 +0800

    Yes or Yes

---
+++ 2178/A/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      String s = sc.next();
+
+      System.out.println(solve(s) ? "YES" : "NO");
+    }
+
+    sc.close();
+  }
+
+  static boolean solve(String s) {
+    return s.chars().filter(c -> c == 'Y').count() <= 1;
+  }
+}
