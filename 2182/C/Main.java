87b2805903c0e165210bbc88dd53ecd0c0fd18ab
Kai Wang <charles.wangkai@gmail.com>
2025-12-30 21:09:13 +0800

    Production of Snowmen

---
+++ 2182/C/Main.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+      int[] b = new int[n];
+      for (int i = 0; i < b.length; ++i) {
+        b[i] = sc.nextInt();
+      }
+      int[] c = new int[n];
+      for (int i = 0; i < c.length; ++i) {
+        c[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a, b, c));
+    }
+
+    sc.close();
+  }
+
+  static long solve(int[] a, int[] b, int[] c) {
+    return (long) a.length * computeWayNum(a, b) * computeWayNum(b, c);
+  }
+
+  static int computeWayNum(int[] sizes1, int[] sizes2) {
+    int n = sizes1.length;
+
+    return (int)
+        IntStream.range(0, n)
+            .filter(
+                offset -> IntStream.range(0, n).allMatch(i -> sizes1[i] < sizes2[(i + offset) % n]))
+            .count();
+  }
+}
