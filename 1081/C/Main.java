18499c57d9d20408e770cc9bfbb32a9669326866
Kai Wang <charles.wangkai@gmail.com>
2026-01-04 15:30:41 +0800

    Colorful Bricks

--- 1081/C/Main.java
+++ 1081/C/Main.java
@@ -1,9 +1,8 @@
 import java.math.BigInteger;
 import java.util.Scanner;
-import java.util.stream.IntStream;
 
 public class Main {
-  static final int MODULUS = 998_244_353;
+  static final ModInt MOD_INT = new ModInt(998_244_353);
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
@@ -18,27 +17,52 @@ public class Main {
   }
 
   static int solve(int n, int m, int k) {
-    return multiplyMod(multiplyMod(m, powMod(m - 1, k)), CMod(n - 1, k));
-  }
-
-  static int multiplyMod(int x, int y) {
-    return Math.floorMod((long) x * y, MODULUS);
-  }
-
-  static int divideMod(int x, int y) {
-    return multiplyMod(x, BigInteger.valueOf(y).modInverse(BigInteger.valueOf(MODULUS)).intValue());
+    return MOD_INT.multiplyMod(MOD_INT.multiplyMod(m, MOD_INT.powMod(m - 1, k)), CMod(n - 1, k));
   }
 
   static int CMod(int n, int r) {
     int result = 1;
     for (int i = 0; i < r; ++i) {
-      result = multiplyMod(result, divideMod(n - i, i + 1));
+      result = MOD_INT.multiplyMod(result, MOD_INT.divideMod(n - i, i + 1));
     }
 
     return result;
   }
+}
 
-  static int powMod(int base, int exponent) {
-    return IntStream.range(0, exponent).reduce(1, (acc, x) -> multiplyMod(acc, base));
+class ModInt {
+  int modulus;
+
+  ModInt(int modulus) {
+    this.modulus = modulus;
+  }
+
+  int mod(long x) {
+    return (int) (x % modulus);
+  }
+
+  int modInv(int x) {
+    return BigInteger.valueOf(x).modInverse(BigInteger.valueOf(modulus)).intValue();
+  }
+
+  int addMod(int x, int y) {
+    return Math.floorMod(x + y, modulus);
+  }
+
+  int multiplyMod(int x, int y) {
+    return Math.floorMod((long) x * y, modulus);
+  }
+
+  int divideMod(int x, int y) {
+    return multiplyMod(x, modInv(y));
+  }
+
+  int powMod(int base, int exponent) {
+    if (exponent == 0) {
+      return 1;
+    }
+
+    return multiplyMod(
+        (exponent % 2 == 0) ? 1 : base, powMod(multiplyMod(base, base), exponent / 2));
   }
 }
