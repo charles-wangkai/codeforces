0cf3744809f54af7a188fd89cb53544f7cec6aed
Kai Wang <charles.wangkai@gmail.com>
2026-01-08 11:06:50 +0800

    Graph Composition

--- 2060/E/Main.java
+++ 2060/E/Main.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
-import java.util.stream.IntStream;
 
 public class Main {
   public static void main(String[] args) {
@@ -31,44 +34,65 @@ public class Main {
   }
 
   static int solve(int n, int[] fu, int[] fv, int[] gu, int[] gv) {
-    int[] gParents = new int[n];
-    Arrays.fill(gParents, -1);
+    Dsu gDsu = new Dsu(n);
     for (int i = 0; i < gu.length; ++i) {
-      int root1 = findRoot(gParents, gu[i] - 1);
-      int root2 = findRoot(gParents, gv[i] - 1);
-      if (root1 != root2) {
-        gParents[root2] = root1;
-      }
+      gDsu.union(gu[i] - 1, gv[i] - 1);
     }
 
     int result = 0;
-    int[] fParents = new int[n];
-    Arrays.fill(fParents, -1);
+    Dsu fDsu = new Dsu(n);
     for (int i = 0; i < fu.length; ++i) {
-      if (findRoot(gParents, fu[i] - 1) != findRoot(gParents, fv[i] - 1)) {
+      if (gDsu.find(fu[i] - 1) != gDsu.find(fv[i] - 1)) {
         ++result;
       } else {
-        int root1 = findRoot(fParents, fu[i] - 1);
-        int root2 = findRoot(fParents, fv[i] - 1);
-        if (root1 != root2) {
-          fParents[root2] = root1;
-        }
+        fDsu.union(fu[i] - 1, fv[i] - 1);
       }
     }
-    result +=
-        IntStream.range(0, n).map(i -> findRoot(fParents, i)).distinct().count()
-            - IntStream.range(0, n).map(i -> findRoot(gParents, i)).distinct().count();
+    result += fDsu.buildLeaderToGroup().size() - gDsu.buildLeaderToGroup().size();
 
     return result;
   }
+}
 
-  static int findRoot(int[] parents, int node) {
-    if (parents[node] == -1) {
-      return node;
+class Dsu {
+  int[] parentOrSizes;
+
+  Dsu(int n) {
+    parentOrSizes = new int[n];
+    Arrays.fill(parentOrSizes, -1);
+  }
+
+  int find(int a) {
+    if (parentOrSizes[a] < 0) {
+      return a;
     }
 
-    parents[node] = findRoot(parents, parents[node]);
+    parentOrSizes[a] = find(parentOrSizes[a]);
 
-    return parents[node];
+    return parentOrSizes[a];
+  }
+
+  void union(int a, int b) {
+    int aLeader = find(a);
+    int bLeader = find(b);
+    if (aLeader != bLeader) {
+      parentOrSizes[aLeader] += parentOrSizes[bLeader];
+      parentOrSizes[bLeader] = aLeader;
+    }
+  }
+
+  int getSize(int a) {
+    return -parentOrSizes[find(a)];
+  }
+
+  Map<Integer, List<Integer>> buildLeaderToGroup() {
+    Map<Integer, List<Integer>> leaderToGroup = new HashMap<>();
+    for (int i = 0; i < parentOrSizes.length; ++i) {
+      int leader = find(i);
+      leaderToGroup.putIfAbsent(leader, new ArrayList<>());
+      leaderToGroup.get(leader).add(i);
+    }
+
+    return leaderToGroup;
   }
 }
