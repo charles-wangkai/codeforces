bc9566168e78b8ab877b54ba21c5409543759c9f
Kai Wang <charles.wangkai@gmail.com>
2026-01-08 10:52:52 +0800

    Round Dance

--- 1833/E/Main.java
+++ 1833/E/Main.java
@@ -1,6 +1,8 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
@@ -26,50 +28,77 @@ public class Main {
   static String solve(int[] a) {
     int n = a.length;
 
-    int[] parents = new int[n];
-    Arrays.fill(parents, -1);
-
     @SuppressWarnings("unchecked")
     Set<Integer>[] adjSets = new Set[n];
     for (int i = 0; i < adjSets.length; ++i) {
       adjSets[i] = new HashSet<>();
     }
 
+    Dsu dsu = new Dsu(n);
     for (int i = 0; i < a.length; ++i) {
-      int root1 = findRoot(parents, i);
-      int root2 = findRoot(parents, a[i] - 1);
-      if (root1 != root2) {
-        parents[root2] = root1;
-      }
+      dsu.union(i, a[i] - 1);
 
       adjSets[i].add(a[i] - 1);
       adjSets[a[i] - 1].add(i);
     }
 
-    Map<Integer, Boolean> groupToFree = new HashMap<>();
+    Map<Integer, Boolean> leaderToFree = new HashMap<>();
     for (int i = 0; i < n; ++i) {
-      int root = findRoot(parents, i);
-      groupToFree.putIfAbsent(root, false);
+      int leader = dsu.find(i);
+      leaderToFree.putIfAbsent(leader, false);
 
       if (adjSets[i].size() == 1) {
-        groupToFree.put(root, true);
+        leaderToFree.put(leader, true);
       }
     }
 
-    return String.format(
-        "%d %d",
-        groupToFree.values().stream().filter(free -> !free).count()
-            + (groupToFree.values().stream().anyMatch(free -> free) ? 1 : 0),
-        groupToFree.size());
+    return "%d %d"
+        .formatted(
+            leaderToFree.values().stream().filter(free -> !free).count()
+                + (leaderToFree.values().stream().anyMatch(free -> free) ? 1 : 0),
+            leaderToFree.size());
+  }
+}
+
+class Dsu {
+  int[] parentOrSizes;
+
+  Dsu(int n) {
+    parentOrSizes = new int[n];
+    Arrays.fill(parentOrSizes, -1);
   }
 
-  static int findRoot(int[] parents, int node) {
-    if (parents[node] == -1) {
-      return node;
+  int find(int a) {
+    if (parentOrSizes[a] < 0) {
+      return a;
     }
 
-    parents[node] = findRoot(parents, parents[node]);
+    parentOrSizes[a] = find(parentOrSizes[a]);
 
-    return parents[node];
+    return parentOrSizes[a];
+  }
+
+  void union(int a, int b) {
+    int aLeader = find(a);
+    int bLeader = find(b);
+    if (aLeader != bLeader) {
+      parentOrSizes[aLeader] += parentOrSizes[bLeader];
+      parentOrSizes[bLeader] = aLeader;
+    }
+  }
+
+  int getSize(int a) {
+    return -parentOrSizes[find(a)];
+  }
+
+  Map<Integer, List<Integer>> buildLeaderToGroup() {
+    Map<Integer, List<Integer>> leaderToGroup = new HashMap<>();
+    for (int i = 0; i < parentOrSizes.length; ++i) {
+      int leader = find(i);
+      leaderToGroup.putIfAbsent(leader, new ArrayList<>());
+      leaderToGroup.get(leader).add(i);
+    }
+
+    return leaderToGroup;
   }
 }
