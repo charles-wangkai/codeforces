16cff1982ef94fbe37f860af8e4ffef764f020a9
Kai Wang <charles.wangkai@gmail.com>
2026-01-08 10:39:56 +0800

    Districts Connection

--- 1433/D/Main.java
+++ 1433/D/Main.java
@@ -1,7 +1,9 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
-import java.util.stream.IntStream;
 
 public class Main {
   public static void main(String[] args) {
@@ -24,40 +26,62 @@ public class Main {
   static String solve(int[] a) {
     int n = a.length;
 
-    int[] parents = IntStream.range(0, n).map(i -> -1).toArray();
     List<String> roads = new ArrayList<>();
+    Dsu dsu = new Dsu(n);
     for (int x = 0; x < n; ++x) {
       for (int y = x + 1; y < n; ++y) {
-        int rootX = findRoot(parents, x);
-        int rootY = findRoot(parents, y);
-        if (a[x] != a[y] && rootX != rootY) {
-          parents[rootY] = rootX;
-          roads.add(String.format("%d %d", x + 1, y + 1));
+        int leader1 = dsu.find(x);
+        int leader2 = dsu.find(y);
+        if (a[x] != a[y] && leader1 != leader2) {
+          dsu.union(leader1, leader2);
+          roads.add("%d %d".formatted(x + 1, y + 1));
         }
       }
     }
 
-    if (roads.size() == n - 1) {
-      return String.format("YES\n%s", String.join("\n", roads));
-    } else {
-      return "NO";
+    return (roads.size() == n - 1) ? "YES\n%s".formatted(String.join("\n", roads)) : "NO";
+  }
+}
+
+class Dsu {
+  int[] parentOrSizes;
+
+  Dsu(int n) {
+    parentOrSizes = new int[n];
+    Arrays.fill(parentOrSizes, -1);
+  }
+
+  int find(int a) {
+    if (parentOrSizes[a] < 0) {
+      return a;
+    }
+
+    parentOrSizes[a] = find(parentOrSizes[a]);
+
+    return parentOrSizes[a];
+  }
+
+  void union(int a, int b) {
+    int aLeader = find(a);
+    int bLeader = find(b);
+    if (aLeader != bLeader) {
+      parentOrSizes[aLeader] += parentOrSizes[bLeader];
+      parentOrSizes[bLeader] = aLeader;
     }
   }
 
-  static int findRoot(int[] parents, int node) {
-    int root = node;
-    while (parents[root] != -1) {
-      root = parents[root];
+  int getSize(int a) {
+    return -parentOrSizes[find(a)];
+  }
+
+  Map<Integer, List<Integer>> buildLeaderToGroup() {
+    Map<Integer, List<Integer>> leaderToGroup = new HashMap<>();
+    for (int i = 0; i < parentOrSizes.length; ++i) {
+      int leader = find(i);
+      leaderToGroup.putIfAbsent(leader, new ArrayList<>());
+      leaderToGroup.get(leader).add(i);
     }
 
-    int p = node;
-    while (p != root) {
-      int next = parents[p];
-      parents[p] = root;
-
-      p = next;
-    }
-
-    return root;
+    return leaderToGroup;
   }
 }
