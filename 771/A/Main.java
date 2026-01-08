cc098200fea15826618189771446aec0ddfa6fd9
Kai Wang <charles.wangkai@gmail.com>
2026-01-08 14:06:27 +0800

    Bear and Friendship Condition

--- 771/A/Main.java
+++ 771/A/Main.java
@@ -1,73 +1,90 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
 
-		int n = sc.nextInt();
-		int m = sc.nextInt();
-		int[] a = new int[m];
-		int[] b = new int[m];
-		for (int i = 0; i < m; i++) {
-			a[i] = sc.nextInt();
-			b[i] = sc.nextInt();
-		}
-		System.out.println(solve(n, a, b) ? "YES" : "NO");
+    int n = sc.nextInt();
+    int m = sc.nextInt();
+    int[] a = new int[m];
+    int[] b = new int[m];
+    for (int i = 0; i < m; i++) {
+      a[i] = sc.nextInt();
+      b[i] = sc.nextInt();
+    }
+    System.out.println(solve(n, a, b) ? "YES" : "NO");
 
-		sc.close();
-	}
+    sc.close();
+  }
 
-	static boolean solve(int n, int[] a, int[] b) {
-		int[] parents = new int[n];
-		Arrays.fill(parents, -1);
+  static boolean solve(int n, int[] a, int[] b) {
+    Dsu dsu = new Dsu(n);
+    for (int i = 0; i < a.length; i++) {
+      dsu.union(a[i] - 1, b[i] - 1);
+    }
 
-		for (int i = 0; i < a.length; i++) {
-			int rootA = findRoot(parents, a[i] - 1);
-			int rootB = findRoot(parents, b[i] - 1);
+    Map<Integer, List<Integer>> leaderToGroup = dsu.buildLeaderToGroup();
 
-			if (rootA != rootB) {
-				parents[rootB] = rootA;
-			}
-		}
+    Map<Integer, Integer> leaderToEdgeCount = new HashMap<>();
+    for (int ai : a) {
+      int leader = dsu.find(ai - 1);
+      leaderToEdgeCount.put(leader, leaderToEdgeCount.getOrDefault(leader, 0) + 1);
+    }
 
-		Map<Integer, Integer> rootToNodeCount = new HashMap<>();
-		for (int i = 0; i < n; i++) {
-			int root = findRoot(parents, i);
+    return leaderToGroup.keySet().stream()
+        .allMatch(
+            leader -> {
+              int nodeCount = leaderToGroup.get(leader).size();
 
-			rootToNodeCount.put(root, rootToNodeCount.getOrDefault(root, 0) + 1);
-		}
+              return nodeCount * (nodeCount - 1L) / 2 == leaderToEdgeCount.getOrDefault(leader, 0);
+            });
+  }
+}
 
-		Map<Integer, Integer> rootToEdgeCount = new HashMap<>();
-		for (int ai : a) {
-			int root = findRoot(parents, ai - 1);
+class Dsu {
+  int[] parentOrSizes;
 
-			rootToEdgeCount.put(root, rootToEdgeCount.getOrDefault(root, 0) + 1);
-		}
+  Dsu(int n) {
+    parentOrSizes = new int[n];
+    Arrays.fill(parentOrSizes, -1);
+  }
 
-		return rootToNodeCount.keySet().stream().allMatch(root -> {
-			int nodeCount = rootToNodeCount.get(root);
+  int find(int a) {
+    if (parentOrSizes[a] < 0) {
+      return a;
+    }
 
-			return nodeCount * (nodeCount - 1L) / 2 == rootToEdgeCount.getOrDefault(root, 0);
-		});
-	}
+    parentOrSizes[a] = find(parentOrSizes[a]);
 
-	static int findRoot(int[] parents, int node) {
-		int root = node;
-		while (parents[root] != -1) {
-			root = parents[root];
-		}
+    return parentOrSizes[a];
+  }
 
-		int p = node;
-		while (p != root) {
-			int next = parents[p];
-			parents[p] = root;
+  void union(int a, int b) {
+    int aLeader = find(a);
+    int bLeader = find(b);
+    if (aLeader != bLeader) {
+      parentOrSizes[aLeader] += parentOrSizes[bLeader];
+      parentOrSizes[bLeader] = aLeader;
+    }
+  }
 
-			p = next;
-		}
+  int getSize(int a) {
+    return -parentOrSizes[find(a)];
+  }
 
-		return root;
-	}
+  Map<Integer, List<Integer>> buildLeaderToGroup() {
+    Map<Integer, List<Integer>> leaderToGroup = new HashMap<>();
+    for (int i = 0; i < parentOrSizes.length; ++i) {
+      int leader = find(i);
+      leaderToGroup.putIfAbsent(leader, new ArrayList<>());
+      leaderToGroup.get(leader).add(i);
+    }
+
+    return leaderToGroup;
+  }
 }
