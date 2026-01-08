856a995c939b6f38c707ba4bd5e9f570c3ee7dc0
Kai Wang <charles.wangkai@gmail.com>
2026-01-08 14:12:05 +0800

    Rumor

--- 893/C/Main.java
+++ 893/C/Main.java
@@ -1,62 +1,91 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
-import java.util.stream.IntStream;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
 
-		int n = sc.nextInt();
-		int m = sc.nextInt();
-		int[] c = new int[n];
-		for (int i = 0; i < c.length; i++) {
-			c[i] = sc.nextInt();
-		}
-		int[] x = new int[m];
-		int[] y = new int[m];
-		for (int i = 0; i < m; i++) {
-			x[i] = sc.nextInt() - 1;
-			y[i] = sc.nextInt() - 1;
-		}
-		System.out.println(solve(c, x, y));
+    int n = sc.nextInt();
+    int m = sc.nextInt();
+    int[] c = new int[n];
+    for (int i = 0; i < c.length; i++) {
+      c[i] = sc.nextInt();
+    }
+    int[] x = new int[m];
+    int[] y = new int[m];
+    for (int i = 0; i < m; i++) {
+      x[i] = sc.nextInt() - 1;
+      y[i] = sc.nextInt() - 1;
+    }
+    System.out.println(solve(c, x, y));
 
-		sc.close();
-	}
+    sc.close();
+  }
 
-	static long solve(int[] c, int[] x, int[] y) {
-		int[] parents = new int[c.length];
-		Arrays.fill(parents, -1);
+  static long solve(int[] c, int[] x, int[] y) {
+    Dsu dsu = new Dsu(c.length);
+    for (int i = 0; i < x.length; i++) {
+      int xLeader = dsu.find(x[i]);
+      int yLeader = dsu.find(y[i]);
+      if (xLeader != yLeader) {
+        if (c[xLeader] <= c[yLeader]) {
+          dsu.union(xLeader, yLeader);
+        } else {
+          dsu.union(yLeader, xLeader);
+        }
+      }
+    }
 
-		for (int i = 0; i < x.length; i++) {
-			int rootX = findRoot(parents, x[i]);
-			int rootY = findRoot(parents, y[i]);
+    return dsu.buildLeaderToGroup().keySet().stream()
+        .mapToInt(leader -> c[leader])
+        .asLongStream()
+        .sum();
+  }
+}
 
-			if (rootX != rootY) {
-				if (c[rootX] <= c[rootY]) {
-					parents[rootY] = rootX;
-				} else {
-					parents[rootX] = rootY;
-				}
-			}
-		}
+class Dsu {
+  int[] parentOrSizes;
 
-		return IntStream.range(0, c.length).filter(i -> parents[i] == -1).map(i -> c[i]).asLongStream().sum();
-	}
+  Dsu(int n) {
+    parentOrSizes = new int[n];
+    Arrays.fill(parentOrSizes, -1);
+  }
 
-	static int findRoot(int[] parents, int node) {
-		int root = node;
-		while (parents[root] != -1) {
-			root = parents[root];
-		}
+  int find(int a) {
+    if (parentOrSizes[a] < 0) {
+      return a;
+    }
 
-		int p = node;
-		while (p != root) {
-			int next = parents[p];
-			parents[p] = root;
+    parentOrSizes[a] = find(parentOrSizes[a]);
 
-			p = next;
-		}
+    return parentOrSizes[a];
+  }
 
-		return root;
-	}
+  void union(int a, int b) {
+    int aLeader = find(a);
+    int bLeader = find(b);
+    if (aLeader != bLeader) {
+      parentOrSizes[aLeader] += parentOrSizes[bLeader];
+      parentOrSizes[bLeader] = aLeader;
+    }
+  }
+
+  int getSize(int a) {
+    return -parentOrSizes[find(a)];
+  }
+
+  Map<Integer, List<Integer>> buildLeaderToGroup() {
+    Map<Integer, List<Integer>> leaderToGroup = new HashMap<>();
+    for (int i = 0; i < parentOrSizes.length; ++i) {
+      int leader = find(i);
+      leaderToGroup.putIfAbsent(leader, new ArrayList<>());
+      leaderToGroup.get(leader).add(i);
+    }
+
+    return leaderToGroup;
+  }
 }
