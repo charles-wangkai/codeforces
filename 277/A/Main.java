4f31e52f7d9015a2552f23b723927b64c6f118cf
Kai Wang <charles.wangkai@gmail.com>
2026-01-08 12:23:05 +0800

    Learning Languages

--- 277/A/Main.java
+++ 277/A/Main.java
@@ -1,73 +1,96 @@
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
-import java.util.Set;
+import java.util.stream.IntStream;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
 
-		int n = sc.nextInt();
-		int m = sc.nextInt();
-		int[][] a = new int[n][];
-		for (int i = 0; i < a.length; i++) {
-			int k = sc.nextInt();
-			a[i] = new int[k];
-			for (int j = 0; j < a[i].length; j++) {
-				a[i][j] = sc.nextInt();
-			}
-		}
-		System.out.println(solve(a, m));
+    int n = sc.nextInt();
+    int m = sc.nextInt();
+    int[][] a = new int[n][];
+    for (int i = 0; i < a.length; ++i) {
+      int k = sc.nextInt();
+      a[i] = new int[k];
+      for (int j = 0; j < a[i].length; ++j) {
+        a[i][j] = sc.nextInt();
+      }
+    }
+    System.out.println(solve(a, m));
 
-		sc.close();
-	}
+    sc.close();
+  }
 
-	static int solve(int[][] a, int m) {
-		boolean[] used = new boolean[m + 1];
-		int[] parents = new int[m + 1];
-		int result = 0;
-		for (int[] ai : a) {
-			if (ai.length == 0) {
-				result++;
-			} else {
-				for (int j = 0; j < ai.length; j++) {
-					used[ai[j]] = true;
+  static int solve(int[][] a, int m) {
+    int result = 0;
+    boolean[] used = new boolean[m + 1];
+    Dsu dsu = new Dsu(m + 1);
+    for (int[] ai : a) {
+      if (ai.length == 0) {
+        ++result;
+      } else {
+        for (int j = 0; j < ai.length; ++j) {
+          used[ai[j]] = true;
 
-					if (j + 1 < ai.length) {
-						int root1 = findRoot(parents, ai[j]);
-						int root2 = findRoot(parents, ai[j + 1]);
+          if (j + 1 < ai.length) {
+            dsu.union(ai[j], ai[j + 1]);
+          }
+        }
+      }
+    }
+    result +=
+        Math.max(
+            0,
+            (int) IntStream.rangeClosed(1, m).filter(i -> used[i]).map(dsu::find).distinct().count()
+                - 1);
 
-						if (root1 != root2) {
-							parents[root2] = root1;
-						}
-					}
-				}
-			}
-		}
+    return result;
+  }
+}
 
-		Set<Integer> roots = new HashSet<>();
-		for (int i = 1; i <= m; i++) {
-			if (used[i]) {
-				roots.add(findRoot(parents, i));
-			}
-		}
-		result += Math.max(0, roots.size() - 1);
+class Dsu {
+  int[] parentOrSizes;
 
-		return result;
-	}
+  Dsu(int n) {
+    parentOrSizes = new int[n];
+    Arrays.fill(parentOrSizes, -1);
+  }
 
-	static int findRoot(int[] parents, int node) {
-		int root = node;
-		while (parents[root] != 0) {
-			root = parents[root];
-		}
+  int find(int a) {
+    if (parentOrSizes[a] < 0) {
+      return a;
+    }
 
-		int p = node;
-		while (p != root) {
-			int next = parents[p];
-			parents[p] = root;
+    parentOrSizes[a] = find(parentOrSizes[a]);
 
-			p = next;
-		}
-		return root;
-	}
+    return parentOrSizes[a];
+  }
+
+  void union(int a, int b) {
+    int aLeader = find(a);
+    int bLeader = find(b);
+    if (aLeader != bLeader) {
+      parentOrSizes[aLeader] += parentOrSizes[bLeader];
+      parentOrSizes[bLeader] = aLeader;
+    }
+  }
+
+  int getSize(int a) {
+    return -parentOrSizes[find(a)];
+  }
+
+  Map<Integer, List<Integer>> buildLeaderToGroup() {
+    Map<Integer, List<Integer>> leaderToGroup = new HashMap<>();
+    for (int i = 0; i < parentOrSizes.length; ++i) {
+      int leader = find(i);
+      leaderToGroup.putIfAbsent(leader, new ArrayList<>());
+      leaderToGroup.get(leader).add(i);
+    }
+
+    return leaderToGroup;
+  }
 }
