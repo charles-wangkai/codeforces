d2bb9758d7c9d12bd9398c9d5218ebd19dae164c
Kai Wang <charles.wangkai@gmail.com>
2026-01-08 12:16:00 +0800

    Roads not only in Berland

--- 25/D/Main.java
+++ 25/D/Main.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -26,39 +28,75 @@ public class Main {
     int n = a.length + 1;
 
     List<Integer> closed = new ArrayList<>();
-    int[] parents = new int[n];
-    Arrays.fill(parents, -1);
+    Dsu dsu = new Dsu(n);
     for (int i = 0; i < a.length; ++i) {
-      int root1 = findRoot(parents, a[i] - 1);
-      int root2 = findRoot(parents, b[i] - 1);
-      if (root1 == root2) {
+      int leader1 = dsu.find(a[i] - 1);
+      int leader2 = dsu.find(b[i] - 1);
+      if (leader1 == leader2) {
         closed.add(i);
       } else {
-        parents[root2] = root1;
+        dsu.union(leader1, leader2);
       }
     }
 
-    int[] roots = IntStream.range(0, n).map(i -> findRoot(parents, i)).distinct().toArray();
+    int[] leaders =
+        dsu.buildLeaderToGroup().keySet().stream().mapToInt(Integer::intValue).toArray();
 
-    return String.format(
-        "%d\n%s",
-        closed.size(),
-        IntStream.range(0, closed.size())
-            .mapToObj(
-                i ->
-                    String.format(
-                        "%d %d %d %d",
-                        a[closed.get(i)], b[closed.get(i)], roots[i] + 1, roots[i + 1] + 1))
-            .collect(Collectors.joining("\n")));
+    return "%d\n%s"
+        .formatted(
+            closed.size(),
+            IntStream.range(0, closed.size())
+                .mapToObj(
+                    i ->
+                        "%d %d %d %d"
+                            .formatted(
+                                a[closed.get(i)],
+                                b[closed.get(i)],
+                                leaders[i] + 1,
+                                leaders[i + 1] + 1))
+                .collect(Collectors.joining("\n")));
+  }
+}
+
+class Dsu {
+  int[] parentOrSizes;
+
+  Dsu(int n) {
+    parentOrSizes = new int[n];
+    Arrays.fill(parentOrSizes, -1);
   }
 
-  static int findRoot(int[] parents, int node) {
-    if (parents[node] == -1) {
-      return node;
+  int find(int a) {
+    if (parentOrSizes[a] < 0) {
+      return a;
     }
 
-    parents[node] = findRoot(parents, parents[node]);
+    parentOrSizes[a] = find(parentOrSizes[a]);
 
-    return parents[node];
+    return parentOrSizes[a];
+  }
+
+  void union(int a, int b) {
+    int aLeader = find(a);
+    int bLeader = find(b);
+    if (aLeader != bLeader) {
+      parentOrSizes[aLeader] += parentOrSizes[bLeader];
+      parentOrSizes[bLeader] = aLeader;
+    }
+  }
+
+  int getSize(int a) {
+    return -parentOrSizes[find(a)];
+  }
+
+  Map<Integer, List<Integer>> buildLeaderToGroup() {
+    Map<Integer, List<Integer>> leaderToGroup = new HashMap<>();
+    for (int i = 0; i < parentOrSizes.length; ++i) {
+      int leader = find(i);
+      leaderToGroup.putIfAbsent(leader, new ArrayList<>());
+      leaderToGroup.get(leader).add(i);
+    }
+
+    return leaderToGroup;
   }
 }
