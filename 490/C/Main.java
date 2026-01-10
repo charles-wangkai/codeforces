4642d1daa2f81d096d52cc50e9cefc96bdd7b4a2
Kai Wang <charles.wangkai@gmail.com>
2026-01-10 10:50:51 +0800

    Hacking Cypher

--- 490/C/Main.java
+++ 490/C/Main.java
@@ -1,60 +1,94 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
 
-		String s = sc.next();
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		System.out.println(solve(s, a, b));
+    String s = sc.next();
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    System.out.println(solve(s, a, b));
 
-		sc.close();
-	}
+    sc.close();
+  }
 
-	static String solve(String s, int a, int b) {
-		int[] leftRemainders = buildLeftRemainders(s, a);
-		int[] rightRemainders = buildRightRemainders(s, b);
+  static String solve(String s, int a, int b) {
+    int[] leftRemainders = buildLeftRemainders(s, a);
+    int[] rightRemainders = buildRightRemainders(s, b);
 
-		for (int i = 0; i < s.length() - 1; i++) {
-			if (s.charAt(i + 1) != '0' && leftRemainders[i] == 0 && rightRemainders[i + 1] == 0) {
-				return String.format("YES\n%s\n%s", s.substring(0, i + 1), s.substring(i + 1));
-			}
-		}
+    for (int i = 0; i < s.length() - 1; i++) {
+      if (s.charAt(i + 1) != '0' && leftRemainders[i] == 0 && rightRemainders[i + 1] == 0) {
+        return String.format("YES\n%s\n%s", s.substring(0, i + 1), s.substring(i + 1));
+      }
+    }
 
-		return "NO";
-	}
+    return "NO";
+  }
 
-	static int addMod(int x, int y, int modulus) {
-		return (x + y) % modulus;
-	}
+  static int[] buildLeftRemainders(String s, int a) {
+    ModInt modInt = new ModInt(a);
 
-	static int multiplyMod(int x, int y, int modulus) {
-		return (int) ((long) x * y % modulus);
-	}
+    int[] result = new int[s.length()];
+    int remainder = 0;
+    for (int i = 0; i < result.length; i++) {
+      remainder = modInt.addMod(modInt.multiplyMod(remainder, 10), s.charAt(i) - '0');
+      result[i] = remainder;
+    }
 
-	static int[] buildLeftRemainders(String s, int a) {
-		int[] result = new int[s.length()];
-		int remainder = 0;
-		for (int i = 0; i < result.length; i++) {
-			remainder = addMod(multiplyMod(remainder, 10, a), s.charAt(i) - '0', a);
-			result[i] = remainder;
-		}
+    return result;
+  }
 
-		return result;
-	}
+  static int[] buildRightRemainders(String s, int b) {
+    ModInt modInt = new ModInt(b);
 
-	static int[] buildRightRemainders(String s, int b) {
-		int[] result = new int[s.length()];
-		int remainder = 0;
-		int placeValue = 1;
-		for (int i = result.length - 1; i >= 0; i--) {
-			remainder = addMod(multiplyMod(placeValue, s.charAt(i) - '0', b), remainder, b);
-			result[i] = remainder;
+    int[] result = new int[s.length()];
+    int remainder = 0;
+    int placeValue = 1;
+    for (int i = result.length - 1; i >= 0; i--) {
+      remainder = modInt.addMod(modInt.multiplyMod(placeValue, s.charAt(i) - '0'), remainder);
+      result[i] = remainder;
 
-			placeValue = multiplyMod(placeValue, 10, b);
-		}
+      placeValue = modInt.multiplyMod(placeValue, 10);
+    }
 
-		return result;
-	}
+    return result;
+  }
+}
+
+class ModInt {
+  int modulus;
+
+  ModInt(int modulus) {
+    this.modulus = modulus;
+  }
+
+  int mod(long x) {
+    return (int) (x % modulus);
+  }
+
+  int modInv(int x) {
+    return BigInteger.valueOf(x).modInverse(BigInteger.valueOf(modulus)).intValue();
+  }
+
+  int addMod(int x, int y) {
+    return Math.floorMod(x + y, modulus);
+  }
+
+  int multiplyMod(int x, int y) {
+    return Math.floorMod((long) x * y, modulus);
+  }
+
+  int divideMod(int x, int y) {
+    return multiplyMod(x, modInv(y));
+  }
+
+  int powMod(int base, long exponent) {
+    if (exponent == 0) {
+      return 1;
+    }
+
+    return multiplyMod(
+        (exponent % 2 == 0) ? 1 : base, powMod(multiplyMod(base, base), exponent / 2));
+  }
 }
