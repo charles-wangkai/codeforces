3e9e606f875c0b32f8ce301ed03d17872a1a3f33
Kai Wang <charles.wangkai@gmail.com>
2026-01-10 20:10:27 +0800

    Binary Array Game

---
+++ 2183/A/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a));
+    }
+
+    sc.close();
+  }
+
+  static String solve(int[] a) {
+    return (a[0] == 1 || a[a.length - 1] == 1) ? "Alice" : "Bob";
+  }
+}
