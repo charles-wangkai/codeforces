cff87d52acf74cfbbfaaa255d58e277051a7e576
Kai Wang <charles.wangkai@gmail.com>
2026-01-14 11:26:15 +0700

    Social Experiment

---
+++ 2184/A/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+
+      System.out.println(solve(n));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int n) {
+    if (n <= 3) {
+      return n;
+    }
+
+    return n % 2;
+  }
+}
