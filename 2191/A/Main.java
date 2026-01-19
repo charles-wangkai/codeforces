f1ebbf944bbce134b668cbc326d8cdbd0fa610e3
Kai Wang <charles.wangkai@gmail.com>
2026-01-19 10:26:28 +0800

    Array Coloring

---
+++ 2191/A/Main.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a) ? "YES" : "NO");
+    }
+
+    sc.close();
+  }
+
+  static boolean solve(int[] a) {
+    int[] sorted = Arrays.stream(a).sorted().toArray();
+    Map<Integer, Integer> valueToSortedIndex =
+        IntStream.range(0, sorted.length).boxed().collect(Collectors.toMap(i -> sorted[i], i -> i));
+
+    return IntStream.range(0, a.length - 1)
+        .allMatch(i -> valueToSortedIndex.get(a[i]) % 2 != valueToSortedIndex.get(a[i + 1]) % 2);
+  }
+}
