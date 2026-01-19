4e2aec4d6388df733177c9b13b32dd7fd186f95a
Kai Wang <charles.wangkai@gmail.com>
2026-01-19 10:46:33 +0800

    MEX Reordering

---
+++ 2191/B/Main.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a) ? "YES" : "NO");
+    }
+
+    sc.close();
+  }
+
+  static boolean solve(int[] a) {
+    int zeroCount = (int) Arrays.stream(a).filter(x -> x == 0).count();
+
+    return zeroCount == 1 || (zeroCount > 1 && Arrays.stream(a).anyMatch(x -> x == 1));
+  }
+}
