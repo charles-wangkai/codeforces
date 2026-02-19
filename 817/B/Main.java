67e607dc741a2945972c1c053b9520e9aee1cf6b
Kai Wang <charles.wangkai@gmail.com>
2026-02-19 21:03:20 +0800

    Makes And The Product

---
+++ 817/B/Main.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+    int[] a = new int[n];
+    for (int i = 0; i < a.length; ++i) {
+      a[i] = sc.nextInt();
+    }
+
+    System.out.println(solve(a));
+
+    sc.close();
+  }
+
+  static long solve(int[] a) {
+    Map<Integer, Integer> valueToCount = new HashMap<>();
+    for (int ai : a) {
+      valueToCount.put(ai, valueToCount.getOrDefault(ai, 0) + 1);
+    }
+
+    int[] chosen = Arrays.stream(a).sorted().limit(3).toArray();
+    if (chosen[2] == chosen[0]) {
+      return C(valueToCount.get(chosen[2]), 3);
+    }
+    if (chosen[2] == chosen[1]) {
+      return C(valueToCount.get(chosen[2]), 2);
+    }
+
+    return C(valueToCount.get(chosen[2]), 1);
+  }
+
+  static long C(int n, int r) {
+    long result = 1;
+    for (int i = 0; i < r; ++i) {
+      result = result * (n - i) / (i + 1);
+    }
+
+    return result;
+  }
+}
