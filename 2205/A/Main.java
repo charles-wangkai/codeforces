a85114041298cb4433547a770afd0e910ce54a65
Kai Wang <charles.wangkai@gmail.com>
2026-02-27 09:57:21 +0800

    Simons and Making It Beautiful

---
+++ 2205/A/Main.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] p = new int[n];
+      for (int i = 0; i < p.length; ++i) {
+        p[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(p));
+    }
+
+    sc.close();
+  }
+
+  static String solve(int[] p) {
+    int maxValueIndex =
+        IntStream.range(0, p.length).filter(i -> p[i] == p.length).findAny().getAsInt();
+    swap(p, 0, maxValueIndex);
+
+    return Arrays.stream(p).mapToObj(String::valueOf).collect(Collectors.joining(" "));
+  }
+
+  static void swap(int[] a, int index1, int index2) {
+    int temp = a[index1];
+    a[index1] = a[index2];
+    a[index2] = temp;
+  }
+}
