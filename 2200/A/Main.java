c2054277a2d848ae0b7c6b48c94abf17f72e8c4d
Kai Wang <charles.wangkai@gmail.com>
2026-02-28 18:24:35 +0800

    Eating Game

--- 2200/A/Main.java
+++ 2200/A/Main.java
@@ -1,23 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 import java.util.stream.IntStream;
 
 public class Main {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+  public static void main(String[] args) throws Throwable {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    int t = sc.nextInt();
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int t = Integer.parseInt(st.nextToken());
     for (int tc = 0; tc < t; ++tc) {
-      int n = sc.nextInt();
+      st = new StringTokenizer(br.readLine());
+      int n = Integer.parseInt(st.nextToken());
       int[] a = new int[n];
+      st = new StringTokenizer(br.readLine());
       for (int i = 0; i < a.length; ++i) {
-        a[i] = sc.nextInt();
+        a[i] = Integer.parseInt(st.nextToken());
       }
 
       System.out.println(solve(a));
     }
-
-    sc.close();
   }
 
   static int solve(int[] a) {
@@ -27,12 +30,13 @@ public class Main {
 
   static int computeWinner(int[] a, int start) {
     int[] rests = a.clone();
+    int sum = Arrays.stream(rests).sum();
     int index = start;
     while (true) {
       if (rests[index] != 0) {
         --rests[index];
-
-        if (Arrays.stream(rests).allMatch(rest -> rest == 0)) {
+        --sum;
+        if (sum == 0) {
           return index;
         }
       }
