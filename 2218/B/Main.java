e4c32079cdffc239511b24293cb0d8fa10394c3e
Kai Wang <charles.wangkai@gmail.com>
2026-04-05 21:03:43 +0800

    The 67th 6-7 Integer Problem

---
+++ 2218/B/Main.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int[] a = new int[7];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int[] a) {
+    return -Arrays.stream(a).sum() + Arrays.stream(a).max().getAsInt() * 2;
+  }
+}
