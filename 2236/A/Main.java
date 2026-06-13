0af7f891010e7c5c12650d3fd2bb41569bc1f84b
Kai Wang <charles.wangkai@gmail.com>
2026-06-13 09:12:21 +0800

    Games on the Train

---
+++ 2236/A/Main.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] h = new int[n];
+      for (int i = 0; i < h.length; ++i) {
+        h[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(h));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int[] h) {
+    return Arrays.stream(h).max().getAsInt() - Arrays.stream(h).min().getAsInt() + 1;
+  }
+}
