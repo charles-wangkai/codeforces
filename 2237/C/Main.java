1824903970c732375c7766288c92cc045e35236a
Kai Wang <charles.wangkai@gmail.com>
2026-06-23 22:49:42 +0800

    Duck Surplus

---
+++ 2237/C/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < a.length; ++i) {
+        a[i] = sc.nextInt();
+      }
+
+      System.out.println(solve(a));
+    }
+
+    sc.close();
+  }
+
+  static long solve(int[] a) {
+    long result = 0;
+    for (int ai : a) {
+      if (ai < result) {
+        result += ai;
+      } else {
+        result = ai;
+      }
+    }
+
+    return result;
+  }
+}
