9f54a5d62dc9ee55107692f88f8ced158f8f438c
Kai Wang <charles.wangkai@gmail.com>
2026-07-01 17:25:04 +0800

    Good times Good times

---
+++ 2241/B/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      int x = sc.nextInt();
+
+      System.out.println(solve(x));
+    }
+
+    sc.close();
+  }
+
+  static int solve(int x) {
+    int result = 1;
+    for (int i = 0; i < String.valueOf(x).length(); ++i) {
+      result *= 10;
+    }
+    ++result;
+
+    return result;
+  }
+}
