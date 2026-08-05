a2535b7e03a05fbda32c95c4738944a29b99de14
Kai Wang <charles.wangkai@gmail.com>
2026-08-05 09:27:16 +0800

    Evanescent

---
+++ 2254/B/Main.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+    for (int tc = 0; tc < t; ++tc) {
+      sc.nextInt();
+      String s = sc.next();
+
+      System.out.println(solve(s));
+    }
+
+    sc.close();
+  }
+
+  static int solve(String s) {
+    int blockNum =
+        (int)
+            IntStream.range(0, s.length())
+                .filter(i -> i == 0 || s.charAt(i) != s.charAt(i - 1))
+                .count();
+
+    if (IntStream.range(1, s.length() - 1)
+        .anyMatch(
+            i ->
+                s.charAt(i) != s.charAt(i - 1)
+                    && s.charAt(i) != s.charAt(i + 1)
+                    && s.charAt(i - 1) == s.charAt(i + 1))) {
+      return blockNum - 2;
+    }
+    if (IntStream.range(1, s.length() - 1)
+        .anyMatch(i -> s.charAt(i) != s.charAt(i - 1) && s.charAt(i) != s.charAt(i + 1))) {
+      return blockNum - 1;
+    }
+
+    return blockNum;
+  }
+}
